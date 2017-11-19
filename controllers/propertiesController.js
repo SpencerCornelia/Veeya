2a9b0a6e0318d65910bad076c7b00f8cf207e25a
Spencer <spencer0cornelia@gmail.com>
2017-11-19 09:33:36 -0800

    adding properties controller

---
+++ controllers/propertiesController.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// GET HTTP to /properties
+// use this for testing to see all properties
+router.get('/', (req,res) => {
+    res.send("GET");
+});
+
+// POST HTTP to /properties/addproperty
+router.post('/addproperty', (req, res, next) => {
+  res.send("POST to add a new property");
+});
+
+// GET HTTP to /properties/id to see a specific property
+router.get('/:id', (req, res, next) => {
+  res.send("GET specific property")
+});
+
+// GET HTTP for sold properties
+router.get('/soldproperties', (req, res, next) => {
+  res.send("GET for sold properties")
+});
+
+module.exports = router;
