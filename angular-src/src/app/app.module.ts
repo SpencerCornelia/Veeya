699da18f0f078880e4a4c91d34fc167ebc255ac2
Spencer <spencer0cornelia@gmail.com>
2017-11-19 19:51:13 -0800

    added functionality for adding a new property from a form, deleting property, and displaying newly added property

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AddPropertyComponent } from './add-property/add-property.component';
+import { ViewPropertiesComponent } from './view-properties/view-properties.component';
+
+import { AddPropertyService } from './services/addProperty.service';
+import { DeletePropertyService } from './services/deleteProperty.service';
+import { GetAllPropertiesService } from './services/getAllProperties.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPropertyComponent,
+    ViewPropertiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AddPropertyService, DeletePropertyService, GetAllPropertiesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
