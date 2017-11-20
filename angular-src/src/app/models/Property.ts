9ca11c776db68f7f4595dac2af4847f9c8ddc064
Spencer <spencer0cornelia@gmail.com>
2017-11-19 19:51:47 -0800

    added Property model

---
+++ angular-src/src/app/models/Property.ts
@@ -0,0 +1,19 @@
+/* Property.ts */
+export interface Property {
+  _id: Number,
+  address: String,
+  city: String,
+  state: String,
+  zipCode: Number,
+  purchasePrice: Number,
+  bedrooms: Number,
+  bathrooms: Number,
+  rehabCostMin: Number,
+  rehabCostMax: Number,
+  afterRepairValue: Number,
+  averageRent: Number,
+  squareFootage: Number,
+  propertyType: String,
+  yearBuilt: Number,
+  status: String
+}
