6039a9c5dbea9b1bf65bd99bd687d1a7334f35ab
Spencer <spencer0cornelia@gmail.com>
2017-11-21 18:56:45 -0800

    changed mongoose.model to db.model in module.exports

--- models/wholesaler.js
+++ models/wholesaler.js
@@ -1,9 +1,20 @@
 // Require mongoose package
 const mongoose = require('mongoose');
 require('mongoose-type-email');
+const bcrypt = require('bcrypt');
+const config = require('../config/database');
+var db = mongoose.createConnection(config.database);
 
 // Define Wholesaler schema with proper attributes
 const WholesalerSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   firstName: {
     type: String,
     required: true
@@ -13,6 +24,7 @@ const WholesalerSchema = mongoose.Schema({
     required: true
   },
   email: {
+    //type: String,
     type: mongoose.SchemaTypes.Email,
     required: true
   },
@@ -29,4 +41,21 @@ const WholesalerSchema = mongoose.Schema({
   */
 });
 
-const Wholesaler = module.exports = mongoose.model('Wholesaler', WholesalerSchema);
+const Wholesaler = module.exports = db.model('Wholesaler', WholesalerSchema);
+
+module.exports.getWholesalerById = function(id, callback) {
+  Wholesaler.FindById(id, callback);
+};
+
+module.exports.addWholesaler = function(newWholesaler, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newWholesaler.password, salt, (err, hash) => {
+      if (err) {
+        throw err;
+      }
+      newWholesaler.password = hash;
+      newWholesaler.save(callback);
+    });
+  });
+  //newWholesaler.save(callback);
+};
