6a73ac6c2b93f7d1963f04a40578f7e26e736d59
Spencer <spencer0cornelia@gmail.com>
2017-11-24 00:15:00 -0500

    added routes for home and addproperty

---
+++ angular-src/src/app/app-routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+
+// Add in components
+import { ViewPropertiesComponent } from './view-properties/view-properties.component';
+import { AddPropertyComponent } from './add-property/add-property.component';
+
+const APP_ROUTES: Routes = [
+  { path: '', redirectTo: '/properties', pathMatch: 'full' },
+  { path: 'properties', component: ViewPropertiesComponent },
+  { path: 'properties/addproperty', component: AddPropertyComponent }
+];
+
+export const Routing = RouterModule.forRoot(APP_ROUTES);
