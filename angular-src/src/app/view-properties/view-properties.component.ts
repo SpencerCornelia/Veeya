803a5a64ef0bd40e79f5477921500dde3d1bad6a
Spencer <spencer0cornelia@gmail.com>
2017-11-25 14:06:14 -0500

    added method for getting properties for wholesaler

--- angular-src/src/app/view-properties/view-properties.component.ts
+++ angular-src/src/app/view-properties/view-properties.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GetAllPropertiesService } from '../services/getAllProperties.service';
 import { DeletePropertyService } from '../services/deleteProperty.service';
+import { GetWholesalerPropertiesService } from '../services/getWholesalerProperties.service';
 import { Property } from '../models/Property';
 
 @Component({
@@ -12,22 +13,29 @@ export class ViewPropertiesComponent implements OnInit {
 
   // array of Property type
   private properties: Property[] = [];
+  private wholesalerProperties: Property[] = [];
 
-  constructor(private getPropertyService: GetAllPropertiesService, private deletePropertyService: DeletePropertyService) { }
+  constructor(private getPropertyService: GetAllPropertiesService, private deletePropertyService: DeletePropertyService, private getWholesalerPropertyService: GetWholesalerPropertiesService) { }
 
   ngOnInit() {
-    this.loadProperties();
+    this.getPropertiesForWholesaler();
   }
 
-  public loadProperties() {
+  public getPropertiesForWholesaler() {
+    // Get all properties for the wholesaler who is logged in
+    this.getWholesalerPropertyService.getWholesalerProperties("5a19be40ac529d148276ee90")
+      .subscribe(response => this.wholesalerProperties = response)
+  }
+
+  public loadAllProperties() {
     // Get all properties from server and update the properties property
-    this.getPropertyService.getAllProperties().subscribe(
-      response => this.properties = response)
+    this.getPropertyService.getAllProperties()
+      .subscribe(response => this.properties = response)
   }
 
   public deleteProperty(property: Property) {
-    this.deletePropertyService.deleteProperty(property._id).subscribe(
-      response => this.properties = this.properties.filter(properties => properties !== property))
+    this.deletePropertyService.deleteProperty(property._id)
+      .subscribe(response => this.properties = this.properties.filter(properties => properties !== property))
   }
 
   public onAddProperty(newProperty) {
