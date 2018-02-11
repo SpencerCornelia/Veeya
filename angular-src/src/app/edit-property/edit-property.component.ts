23783a811f05b25a8664e7e4c7676845cb3ead13
Spencer <spencer0cornelia@gmail.com>
2018-02-11 00:15:53 -0800

    grab id from params via ActivatedRoute + created getProperty function which calls edit property service

--- angular-src/src/app/edit-property/edit-property.component.ts
+++ angular-src/src/app/edit-property/edit-property.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Property } from '../models/Property';
 import { AppRoutingModule } from '../app-routing.module';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { EditPropertyService } from '../services/editProperty.service';
 
@@ -12,14 +12,47 @@ import { EditPropertyService } from '../services/editProperty.service';
 })
 export class EditPropertyComponent implements OnInit {
 
-  constructor(private currentProperty: Property, private editPropertyService: EditPropertyService, private router: Router) { }
+  private propertyID: string;
+  private initialProperty: Property[];
+
+  constructor(private route: ActivatedRoute, private editPropertyService: EditPropertyService, private router: Router) {
+    this.propertyID = route.snapshot.params['id'];
+    this.getProperty(this.propertyID);
+  }
+
+  getProperty(id) {
+    this.editPropertyService.getPropertyByID(id)
+      .subscribe(response => this.initialProperty = response);
+
+    console.log("this.initialProperty after getPropertyById call =", this.initialProperty);
+  }
 
   ngOnInit() {
-    console.log("currentProperty =", this.currentProperty);
+
     /* Need to receive the property that was clicked from the click function
        and set the default values of the form to the values from the property
        being passed to this component
      */
+    // this.initialProperty = {
+    //   _id: this.currentProperty._id,
+    //   wholesaler: '5a19be40ac529d148276ee90',
+    //   address: this.currentProperty.address,
+    //   city: this.currentProperty.city,
+    //   state: this.currentProperty.state,
+    //   zipCode: this.currentProperty.zipCode,
+    //   purchasePrice: this.currentProperty.purchasePrice,
+    //   bedrooms: this.currentProperty.bedrooms,
+    //   bathrooms: this.currentProperty.bathrooms,
+    //   rehabCostMin: this.currentProperty.rehabCostMin,
+    //   rehabCostMax: this.currentProperty.rehabCostMax,
+    //   afterRepairValue: this.currentProperty.afterRepairValue,
+    //   averageRent: this.currentProperty.averageRent,
+    //   squareFootage: this.currentProperty.squareFootage,
+    //   propertyType: this.currentProperty.propertyType,
+    //   yearBuilt: this.currentProperty.yearBuilt,
+    //   status: this.currentProperty.status,
+    //   comps: this.currentProperty.comps
+    // }
   }
 
 }
