572eb8bfe579b043753cc8cc0fe951e20b6dbd64
Spencer <spencer0cornelia@gmail.com>
2018-02-11 09:49:44 -0800

    this.initialProperty properly set after call to services

--- angular-src/src/app/edit-property/edit-property.component.ts
+++ angular-src/src/app/edit-property/edit-property.component.ts
@@ -13,7 +13,7 @@ import { EditPropertyService } from '../services/editProperty.service';
 export class EditPropertyComponent implements OnInit {
 
   private propertyID: string;
-  private initialProperty: Property[];
+  private initialProperty;
 
   constructor(private route: ActivatedRoute, private editPropertyService: EditPropertyService, private router: Router) {
     this.propertyID = route.snapshot.params['id'];
@@ -22,37 +22,12 @@ export class EditPropertyComponent implements OnInit {
 
   getProperty(id) {
     this.editPropertyService.getPropertyByID(id)
-      .subscribe(response => this.initialProperty = response);
-
-    console.log("this.initialProperty after getPropertyById call =", this.initialProperty);
+      .subscribe((response) => {
+        this.initialProperty = response;
+    });
   }
 
   ngOnInit() {
-
-    /* Need to receive the property that was clicked from the click function
-       and set the default values of the form to the values from the property
-       being passed to this component
-     */
-    // this.initialProperty = {
-    //   _id: this.currentProperty._id,
-    //   wholesaler: '5a19be40ac529d148276ee90',
-    //   address: this.currentProperty.address,
-    //   city: this.currentProperty.city,
-    //   state: this.currentProperty.state,
-    //   zipCode: this.currentProperty.zipCode,
-    //   purchasePrice: this.currentProperty.purchasePrice,
-    //   bedrooms: this.currentProperty.bedrooms,
-    //   bathrooms: this.currentProperty.bathrooms,
-    //   rehabCostMin: this.currentProperty.rehabCostMin,
-    //   rehabCostMax: this.currentProperty.rehabCostMax,
-    //   afterRepairValue: this.currentProperty.afterRepairValue,
-    //   averageRent: this.currentProperty.averageRent,
-    //   squareFootage: this.currentProperty.squareFootage,
-    //   propertyType: this.currentProperty.propertyType,
-    //   yearBuilt: this.currentProperty.yearBuilt,
-    //   status: this.currentProperty.status,
-    //   comps: this.currentProperty.comps
-    // }
   }
 
 }
