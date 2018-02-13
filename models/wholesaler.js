fa49841a11cd75a9c2bcfa8219ba8855001f6875
Spencer <spencer0cornelia@gmail.com>
2018-02-12 23:22:02 -0800

    added function for updating property for wholesaler after edit form is submitted

--- models/wholesaler.js
+++ models/wholesaler.js
@@ -71,11 +71,50 @@ module.exports.addInvestorToWholesaler = function(newInvestor, wholesalerID, cal
       {safe: true, upsert: true},
       function(err, s) {
         if (err) {
-          console.log("err:", err);
+          console.log("error adding investor to wholesaler:", err);
         }
       }
     );
   }
   newInvestor.save(callback);
+};
+
+module.exports.updatePropertyForWholesaler = function(property, callback) {
+  Wholesaler.findOne({_id: property.wholesaler}, (err, wholesaler) => {
+    if (err) {
+      console.log("error finding wholesaler:", err);
+    } else {
+      var prop = wholesaler.properties.filter((p) => {
+        return p._id == property._id;
+      });
+      prop[0] = property;
+      callback(prop[0]);
+    }
+  });
+  // Wholesaler.findOneAndUpdate(
+  //   {_id: property._id},
+  //   {
+  //     address: property.address,
+  //     city: property.city,
+  //     state: property.state,
+  //     zipCode: property.zipCode,
+  //     purchasePrice: property.purchasePrice,
+  //     bedrooms: property.bedrooms,
+  //     bathrooms: property.bathrooms,
+  //     rehabCostMin: property.rehabCostMin,
+  //     rehabCostMax: property.rehabCostMax,
+  //     afterRepairValue: property.afterRepairValue,
+  //     averageRent: property.averageRent,
+  //     squareFootage: property.squareFootage,
+  //     propertyType: property.propertyType,
+  //     yearBuilt: property.yearBuilt,
+  //     status: property.status,
+  //     comps: property.comps
+  //   },
+  //   {
+  //     runValidators: true
+  //   },
+  //   callback()
+  // );
 };
 
