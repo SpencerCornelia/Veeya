c2c9a47c00c1f112eb5929d846c0a0e909d9bcc5
Spencer <spencer0cornelia@gmail.com>
2018-02-16 05:53:49 -0800

    inviteInvestor makes successful post

--- angular-src/src/app/services/inviteInvestor.service.ts
+++ angular-src/src/app/services/inviteInvestor.service.ts
@@ -10,13 +10,21 @@ export class InviteInvestorService {
 
   constructor(private http: Http) { }
 
-  private serverApi = 'http://localhost:3000/inviteinvestor';
+  private serverApi = 'http://localhost:3000/investor';
 
   public inviteInvestor(investor: Investor) {
-    let URI = this.serverApi + "/addinvestor";
+    let URI = this.serverApi + "/inviteinvestor";
     let headers = new Headers;
     let body = JSON.stringify({
-
+      _id: 0,
+      userType: investor.userType,
+      userName: investor.userName,
+      password: investor.password,
+      firstName: investor.firstName,
+      lastName: investor.lastName,
+      email: investor.email,
+      phoneNumber: investor.phoneNumber,
+      wholesaler: investor.wholesaler
     });
     headers.append('Content-Type', 'application/json');
     return this.http.post(URI, body, {headers: headers})
