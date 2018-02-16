e7e13e91d08d705045db2c7f5d56740774dc0a95
Spencer <spencer0cornelia@gmail.com>
2018-02-16 05:55:50 -0800

    successful onSubmit implemented. after invite investor call is successful, user is redirected back to /properties

--- angular-src/src/app/invite-investor/invite-investor.component.ts
+++ angular-src/src/app/invite-investor/invite-investor.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Investor } from '../models/Investor';
+import { AppRoutingModule } from '../app-routing.module';
+import { Router } from '@angular/router';
+import { InviteInvestorService } from '../services/inviteInvestor.service';
+import { ModuleWithProviders } from '@angular/core';
 
 @Component({
   selector: 'app-invite-investor',
@@ -7,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InviteInvestorComponent implements OnInit {
 
-  constructor() { }
+  private newInvestor: Investor;
+  @Output() addInvitedInvestor: EventEmitter<Investor> = new EventEmitter<Investor>();
+
+  constructor(private inviteInvestorService: InviteInvestorService, private router: Router) { }
 
   ngOnInit() {
-    // comment to add to git
+    this.newInvestor = {
+      userType: 'Investor',
+      userName: '',
+      password: 'initialPassword',
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+      wholesaler: '5a19be40ac529d148276ee90'
+    }
+  }
+
+  public onSubmit() {
+    this.inviteInvestorService.inviteInvestor(this.newInvestor)
+      .subscribe((response) => {
+        if (response.success === true) {
+          this.addInvitedInvestor.emit(this.newInvestor);
+          this.router.navigate(['/properties']);
+        }
+      });
   }
 
 }
