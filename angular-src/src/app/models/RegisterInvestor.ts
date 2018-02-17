97c88900894a29fb41a5731cce0ce229d54ccc99
Spencer <spencer0cornelia@gmail.com>
2018-02-16 22:02:05 -0800

    added

---
+++ angular-src/src/app/models/RegisterInvestor.ts
@@ -0,0 +1,9 @@
+export interface RegisterInvestor {
+  userType: String,
+  userName: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  phoneNumber: String
+}
