c78053520b365c9c0766b8cebebbd868fcda5ac1
Spencer <spencer0cornelia@gmail.com>
2018-02-16 22:02:17 -0800

    added

---
+++ angular-src/src/app/models/RegisterWholesaler.ts
@@ -0,0 +1,9 @@
+export interface RegisterWholesaler {
+  userType: String,
+  userName: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  phoneNumber: String
+}
