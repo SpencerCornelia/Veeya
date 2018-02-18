00b7bf416f70c8de6d06086fd96aff92d3bf0dc5
Spencer <spencer0cornelia@gmail.com>
2018-02-18 11:44:37 -0800

    added functionality for registering a new investor

--- models/investor.js
+++ models/investor.js
@@ -2,7 +2,9 @@
 const mongoose = require('mongoose');
 require('mongoose-type-email');
 const config = require('../config/database');
-var db = mongoose.createConnection(config.database);
+const db = mongoose.createConnection(config.database);
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // Require models
 const Wholesaler = require('./wholesaler');
@@ -47,6 +49,53 @@ const InvestorSchema = mongoose.Schema({
 });
 
 const Investor = module.exports = db.model('Investor', InvestorSchema);
+
+module.exports.registerInvestor = function(investor, callback) {
+  Investor.findOne({ 'email': investor.email }, (err, user) => {
+    if (err) {
+      callback(true, {
+        success: false,
+        message: "Error registering investor."
+      });
+    } else if (user) {
+      callback(true, {
+        success: false,
+        message: "Investor already exists"
+      });
+    } else {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(investor.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          }
+          let newInvestor = new Investor({
+            userType: "Investor",
+            userName: investor.userName,
+            password: hash,
+            firstName: investor.firstName,
+            lastName: investor.lastName,
+            email: investor.email,
+            phoneNumber: investor.phoneNumber
+          });
+
+          newInvestor.save((err) => {
+            if (err) {
+              callback(true, {
+                success: false,
+                message: "Error registering investor."
+              });
+            } else {
+              callback(false, {
+                success: true,
+                message: "Successfully registered investor."
+              });
+            }
+          });
+        })
+      });
+    }
+  });
+};
 
 module.exports.inviteInvestor = function(newInvestor, callback) {
   newInvestor.save(callback);
