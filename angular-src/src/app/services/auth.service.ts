66aa1a1585076272b8e1b5d7eca4aaba3b2bb328
Spencer <spencer0cornelia@gmail.com>
2018-02-19 13:00:25 -0800

    implemented promise to handle 500 status codes

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
+
+
+import { HttpErrorResponse } from '@angular/common/http';
+
+
 
 @Injectable()
 export class AuthService {
@@ -10,15 +16,25 @@ export class AuthService {
 
   constructor(private http: Http) { }
 
+  handleError(error: any): Promise<any> {
+    return Promise.reject(error);
+  }
+
   registerUser(user) {
     let serverApi = "http://localhost:3000/";
     let userType = user.userType;
-    let route = serverApi + 'login' + userType;
+    let route = serverApi + 'register/' + userType;
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
+    // return this.http.post(route, user, { headers: headers })
+    //   .map((res) => {
+    //     return res.json();
+    //   });
     return this.http.post(route, user, { headers: headers })
-      .map((res) => {
-        res.json();
-      });
+      .toPromise()
+      .then((response) => {
+        response.json();
+      })
+      .catch(this.handleError)
   }
 }
