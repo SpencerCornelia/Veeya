ef0d40f652d99cd2c5be5333c611c0bcc453ff0c
Spencer <spencer0cornelia@gmail.com>
2018-02-19 10:06:00 -0800

    added functionality for registering user from form

--- angular-src/src/app/register/register.component.ts
+++ angular-src/src/app/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterWholesaler } from '../models/RegisterWholesaler';
+import { ValidateService } from '../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  private newUser: RegisterWholesaler;
+
+  constructor(private validateService: ValidateService,
+              private flashMessage: FlashMessagesService,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.newUser = {
+      userType: 'Investor',
+      userName: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      email: '',
+      phoneNumber: ''
+    }
+  }
+
+  onRegisterSubmit() {
+    // required fields
+    if(!this.validateService.validateRegister(this.newUser)) {
+      this.flashMessage.show('Please fill in all fields.', {
+        cssClass: 'alert-danger',
+        timeout: 2000
+      });
+      return false;
+    }
+
+    // validate email
+    if (!this.validateService.validateEmail(this.newUser.email)) {
+      this.flashMessage.show('Please fill in a valid email.', {
+        cssClass: 'alert-danger',
+        timeout: 2000
+      });
+      return false;
+    }
+
+    // register user
+    this.authService.registerUser(this.newUser)
+      .subscribe((data) => {
+        console.log("data:", data);
+      })
   }
 
 }
