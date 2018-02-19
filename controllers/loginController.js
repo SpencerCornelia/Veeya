08a4c9f77e6a5bb1be0234704dc786f6ef309931
Spencer <spencer0cornelia@gmail.com>
2018-02-18 22:24:58 -0800

    added posting to login for wholesaler and investor

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,15 +1,98 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
+const Wholesaler = require('../models/wholesaler.js');
+const Investor = require('../models/investor.js');
 
-// GET HTTP to /login
-router.get('/', (req,res) => {
-  res.send("get to /login worked");
+const keys = require('../config/keys.js');
+
+// POST HTTP to /login for wholesaler
+router.post('/wholesaler', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  Wholesaler.getWholesalerByEmail(email, (error, response) => {
+    if (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Wholesaler with those credentials not found."
+      });
+    }
+
+    Wholesaler.comparePassword(password, response.wholesaler.password, (error, isMatch) => {
+      if (error) {
+        return res.status(500).json({
+          success: false,
+          message: "Error logging wholesaler into application."
+        });
+      }
+
+      if (isMatch) {
+        const token = jwt.sign(response.wholesaler.toJSON(), keys.secret, {
+          expiresIn: 604800
+        });
+
+        res.status(201).json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: response.wholesaler._id,
+            firstName: response.wholesaler.firstName
+          }
+        });
+      } else {
+        return res.status(500).json({
+          success: false,
+          message: "Combination of email and password incorrect for wholesaler user type."
+        });
+      }
+    });
+  });
 });
 
-// POST HTTP to /login
-router.post('/', (req, res, next) => {
+// POST HTTP login to investor
+router.post('/investor', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
+  Investor.getInvestorByEmail(email, (error, response) => {
+    if (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Investor with those credentials not found."
+      });
+    }
+
+    Investor.comparePassword(password, response.investor.password, (error, isMatch) => {
+      if (error) {
+        return res.status(500).json({
+          success: false,
+          message: "Error logging investor into application."
+        });
+      }
+
+      if (isMatch) {
+        const token = jwt.sign(response.investor.toJSON(), keys.secret, {
+          expiresIn: 604800
+        });
+
+        res.status(201).json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: response.investor._id,
+            firstName: response.investor.firstName
+          }
+        });
+      } else {
+        return res.status(500).json({
+          success: false,
+          message: "Combination of email and password incorrect for investor user type."
+        });
+      }
+    });
+  });
 });
 
 
