6f319146291903bb7e396e8e955dff8b98d60a22
Spencer <spencer0cornelia@gmail.com>
2018-02-19 10:05:23 -0800

    added AuthService and ValidateService

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AddPropertyComponent } from './add-property/add-property.component';
 import { EditPropertyComponent } from './edit-property/edit-property.component';
@@ -15,12 +16,12 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { InviteInvestorComponent } from './invite-investor/invite-investor.component';
 
 import { AddPropertyService } from './services/addProperty.service';
+import { AuthService } from './services/auth.service';
 import { DeletePropertyService } from './services/deleteProperty.service';
 import { EditPropertyService } from './services/editProperty.service';
 import { GetAllPropertiesService } from './services/getAllProperties.service';
 import { InviteInvestorService } from './services/inviteInvestor.service';
-import { LoginService } from './services/login.service';
-import { RegisterService } from './services/register.service';
+import { ValidateService } from './services/validate.service';
 import { GetWholesalerPropertiesService } from './services/getWholesalerProperties.service';
 
 @NgModule({
@@ -38,16 +39,17 @@ import { GetWholesalerPropertiesService } from './services/getWholesalerProperti
     BrowserModule,
     HttpModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlashMessagesModule.forRoot()
   ],
   providers: [
     AddPropertyService,
+    AuthService,
     DeletePropertyService,
     EditPropertyService,
     GetAllPropertiesService,
     InviteInvestorService,
-    LoginService,
-    RegisterService,
+    ValidateService,
     GetWholesalerPropertiesService
   ],
   bootstrap: [AppComponent]
