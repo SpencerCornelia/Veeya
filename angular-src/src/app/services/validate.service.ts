c5aa71a56e465950c73eb0831d6013e52f1dc4d8
Spencer <spencer0cornelia@gmail.com>
2018-02-19 10:04:26 -0800

    functionality added for validating register form for empty fields and validating email

---
+++ angular-src/src/app/services/validate.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ValidateService {
+
+  constructor(private http: Http) { }
+
+  validateRegister(user) {
+    if (!user.firstName || !user.lastName || !user.email || !user.firstName) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  validateEmail(email) {
+    let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
+
+}
