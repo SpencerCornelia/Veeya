4e39b60af78c425fff71ac16b7ea1e9e79c330e4
Spencer <spencer0cornelia@gmail.com>
2018-02-19 17:22:00 -0800

    submit form call to authenticate user returns data. next step is handling 500 status code errors from server

--- angular-src/src/app/login/login.component.ts
+++ angular-src/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private loginUser: any;
+
+  constructor(private authService: AuthService, private flashMessage: FlashMessagesService, private router: Router) { }
 
   ngOnInit() {
+    this.loginUser = {
+      email: '',
+      password: '',
+      userType: 'Investor'
+    }
+  }
+
+  onLoginSubmit() {
+    this.authService.authenticateUser(this.loginUser)
+      .subscribe(data => {
+        console.log("data:", data)
+      });
   }
 
 }
