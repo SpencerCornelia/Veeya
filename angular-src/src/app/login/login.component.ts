3bd0621fe06d90a8b36b5cd60d83ebc3c42b200a
Spencer <spencer0cornelia@gmail.com>
2018-02-20 05:09:04 -0800

    added login submit function to handle logging user in

--- angular-src/src/app/login/login.component.ts
+++ angular-src/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { ValidateService } from '../services/validate.service';
 import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
@@ -12,7 +13,10 @@ export class LoginComponent implements OnInit {
 
   private loginUser: any;
 
-  constructor(private authService: AuthService, private flashMessage: FlashMessagesService, private router: Router) { }
+  constructor(private authService: AuthService,
+              private flashMessage: FlashMessagesService,
+              private router: Router,
+              private validateService: ValidateService) { }
 
   ngOnInit() {
     this.loginUser = {
@@ -23,9 +27,44 @@ export class LoginComponent implements OnInit {
   }
 
   onLoginSubmit() {
+    if(!this.validateService.validateLogin(this.loginUser)) {
+      this.flashMessage.show('Please enter valid info for all fields.', {
+        cssClass: 'alert-danger',
+        timeout: 3000
+      });
+      return false;
+    }
+
+    if (!this.validateService.validateEmail(this.loginUser.email)) {
+      this.flashMessage.show('Please enter a valid login.', {
+        cssClass: 'alert-danger',
+        timeout: 3000
+      });
+      return false;
+    }
+
+
     this.authService.authenticateUser(this.loginUser)
-      .subscribe(data => {
-        console.log("data:", data)
+      .then(response => {
+        let successMessage = 'Welcome back ' + response.user.firstName;
+        this.authService.storeUserData(response.token, response.user.id);
+        this.flashMessage.show(successMessage, {
+          cssClass: 'alert-success',
+          timeout: 3000
+        });
+        this.router.navigate(['/properties']);
+      },
+      error => {
+        let errorBody = JSON.parse(error._body);
+        let errorMessage = errorBody.message;
+        this.loginUser = {
+          email: '',
+          password: ''
+        }
+        this.flashMessage.show(errorMessage, {
+          cssClass: 'alert-danger',
+          timeout: 3000
+        })
       });
   }
 
