ff6062a578b0878d597a3986cc11a473767229e4
Spencer <spencer0cornelia@gmail.com>
2018-02-21 23:30:17 -0800

    route for adding investor connection added

--- controllers/wholesalerController.js
+++ controllers/wholesalerController.js
@@ -43,34 +43,6 @@ router.get('/:uid', (req,res) => {
   });
 });
 
-// POST HTTP to /wholesaler/wholesalerId/addInvestor
-router.post('/:uid/addInvestor', (req, res, next) => {
-  var newInvestor = new investor({
-    userType: req.body.userType,
-    userName: req.body.userName,
-    password: req.body.password,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber
-  });
-  wholesaler.addInvestorToWholesaler(newInvestor, req.params.uid, (error, response) => {
-    if (error) {
-      res.status(500).json({
-        success: response.success,
-        message: response.message,
-        error: response.error
-      });
-    } else {
-      res.status(201).json({
-        success: response.success,
-        message: response.message,
-        investor: response.data
-      });
-    }
-  });
-});
-
 // POST HTTP to register a new wholesaler
 router.post('/register', (req, res) => {
   wholesaler.registerWholesaler(req.body, (error, response) => {
@@ -92,62 +64,40 @@ router.post('/register', (req, res) => {
 
 // POST HTTP to invite a wholesaler
 router.post('/invitewholesaler', (req, res) => {
-  let investor_id = req.body.investor_id;
-  investor.getInvestorById(req.body.investor_id, (error, response) => {
+  wholesaler.registerWholesaler(req.body, (error, response) => {
     if (error) {
       res.status(500).json({
         success: response.success,
         message: response.message,
         error: response.error
       });
-    }
-
-    let newWholesaler = {
-      userType: req.body.userType,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: req.body.password,
-      userName: req.body.userName,
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber
-    }
-    wholesaler.registerWholesaler(newWholesaler, (error, registerWholesalerResponse) => {
-      if (error) {
-        res.status(500).json({
-          success: registerWholesalerResponse.success,
-          message: registerWholesalerResponse.message,
-          error: registerWholesalerResponse.error
-        });
-      }
-
-      delete registerWholesalerResponse.data.password;
-
-      investor.updateWholesalersList(investor_id, registerWholesalerResponse.data, (error, updateWholesalersListResponse) => {
-        if (error) {
-          res.status(500).json({
-            success: updateWholesalersListResponse.success,
-            message: updateWholesalersListResponse.message,
-            error: updateWholesalersListResponse.error
-          });
-        }
+    } else {
+      res.status(201).json({
+        success: response.success,
+        message: response.message,
+        wholesaler: response.data
       });
+    }
+  });
+});
 
-      wholesaler.updateInvestorsList(registerWholesalerResponse._id, response.data, (error, updateInvestorsListResponse) => {
-        if (error) {
-          res.status(500).json({
-            success: updateInvestorsListResponse.success,
-            message: updateInvestorsListResponse.message,
-            error: updateInvestorsListResponse.error
-          });
-        } else {
-          res.status(201).json({
-            success: true,
-            message: 'Successfully invited wholesaler.'
-          });
-        }
-
+// POST HTTP to add investor to investors array
+router.post('/addInvestorConnection', (req, res) => {
+  delete req.body.password;
+  wholesaler.addInvestorConnection(req.body, req.body.wholesaler_id, (error, response) => {
+    if (error) {
+      res.status(500).json({
+        success: response.success,
+        message: response.message,
+        error: response.error
       });
-    });
+    } else {
+      res.status(201).json({
+        success: response.success,
+        message: response.message,
+        wholesaler: response.data
+      });
+    }
   });
 });
 
