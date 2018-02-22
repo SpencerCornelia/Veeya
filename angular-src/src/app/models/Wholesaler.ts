6d4c0db496c0205a330f50046a92572353400a0a
Spencer <spencer0cornelia@gmail.com>
2018-02-21 20:12:17 -0800

    added field for investor_id which is used when inviting a wholesaler

---
+++ angular-src/src/app/models/Wholesaler.ts
@@ -0,0 +1,11 @@
+export interface Wholesaler {
+  _id?: Number,
+  userType: String,
+  firstName: String,
+  lastName: String,
+  password: String,
+  userName: String,
+  email: String,
+  phoneNumber: String,
+  investor_id?: String
+}
