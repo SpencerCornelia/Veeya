f08b7ea5e6b06b66ec0e68ffa42e98aaeb0ea67a
Spencer <spencer0cornelia@gmail.com>
2018-02-21 20:09:57 -0800

    update

---
+++ angular-src/src/app/services/invitewholesaler.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Wholesaler } from '../models/wholesaler.js';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class InviteWholesalerService {
+
+  constructor(private http: Http) { }
+
+  private serverApi = 'http://localhost:3000/wholesaler';
+
+  public inviteWholesaler(wholesaler: Wholesaler) {
+    let URI = this.serverApi + "/invitewholesaler";
+    let headers = new Headers;
+    let body = JSON.stringify({
+      _id: 0,
+      userType: wholesaler.userType,
+      userName: wholesaler.userName,
+      password: wholesaler.password,
+      firstName: wholesaler.firstName,
+      lastName: wholesaler.lastName,
+      email: wholesaler.email,
+      phoneNumber: wholesaler.phoneNumber,
+      investor_id: wholesaler.investor_id
+    });
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(URI, body, {headers: headers})
+      .map((response) => {
+        return response.json()
+      })
+      .catch((error) => {
+        return Observable.throw(error.json());
+      })
+  }
+}
