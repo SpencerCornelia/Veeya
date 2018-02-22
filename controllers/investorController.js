876c384c0d6de249a1831f1bdf4aa6dfddad58f2
Spencer <spencer0cornelia@gmail.com>
2018-02-21 23:30:40 -0800

    route for adding wholesaler connection added

--- controllers/investorController.js
+++ controllers/investorController.js
@@ -65,74 +65,7 @@ router.get('/:uid', (req, res) => {
 
 // POST HTTP to /investor/inviteinvestor
 router.post('/inviteinvestor', (req, res, next) => {
-  let wholesaler_id = req.body.wholesaler_id;
-  wholesaler.getWholesalerById(req.body.wholesaler_id, (error, response) => {
-    if (error) {
-      res.status(500).json({
-        success: response.success,
-        message: response.message,
-        error: response.error
-      });
-    }
-
-    let newInvestor = {
-      userType: req.body.userType,
-      userName: req.body.userName,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      phoneNumber: req.body.phoneNumber
-    }
-    investor.registerInvestor(newInvestor, (error, registerResponse) => {
-      if (error) {
-        res.status(500).json({
-          success: registerResponse.success,
-          message: registerResponse.message,
-          error: registerResponse.error
-        });
-      }
-
-      delete registerResponse.data.password;
-
-      wholesaler.updateInvestorsList(wholesaler_id, registerResponse.data, (error, updateWholesalerResponse) => {
-        if (error) {
-          res.status(500).json({
-            success: updateWholesalerResponse.success,
-            message: updateWholesalerResponse.message,
-            error: updateWholesalerResponse.error
-          });
-        }
-      });
-
-      investor.updateWholesalersList(registerResponse._id, response.data, (error, updateInvestorResponse) => {
-        if (error) {
-          res.status(500).json({
-            success: updateInvestorResponse.success,
-            message: updateInvestorResponse.message,
-            error: updateInvestorResponse.error
-          });
-        } else {
-          res.status(201).json({
-            success: true,
-            message: 'Successfully invited investor.'
-          });
-        }
-      });
-    });
-  });
-});
-
-// GET HTTP for sold properties
-router.get('/sold', (req, res, next) => {
-  res.send("GET for sold properties")
-});
-
-// DELETE HTTP request for deleting a property
-router.delete('/:id', (req, res, next) => {
-  let id = req.params.id;
-
-  property.deletePropertyById(id, (error, response) => {
+  investor.registerInvestor(req.body, (error, response) => {
     if (error) {
       res.status(500).json({
         success: response.success,
@@ -142,7 +75,26 @@ router.delete('/:id', (req, res, next) => {
     } else {
       res.status(201).json({
         success: response.success,
-        message: response.message
+        message: response.message,
+        investor: response.data
+      });
+    }
+  });
+});
+
+router.post('/addwholesalerconnection', (req, res) => {
+  investor.addwholesalerconnection(req.body, req.body.investor_id, (error, response) => {
+    if (error) {
+      res.status(500).json({
+        success: response.success,
+        message: response.message,
+        error: response.error
+      });
+    } else {
+      res.status(201).json({
+        success: response.success,
+        message: response.message,
+        investor: response.data
       });
     }
   });
