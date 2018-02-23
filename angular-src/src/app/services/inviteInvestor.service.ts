d20d1e8190a754e0b655d037757e7db114b6b215
Spencer <spencer0cornelia@gmail.com>
2018-02-22 17:55:12 -0800

    removed unnecessary functions - addWholesalerConnection and addInvestorConnection after refactoring for promises in model/controller

--- angular-src/src/app/services/inviteInvestor.service.ts
+++ angular-src/src/app/services/inviteInvestor.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Investor } from '../models/Investor';
+import { Wholesaler } from '../models/Wholesaler';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
@@ -11,10 +12,10 @@ export class InviteInvestorService {
 
   constructor(private http: Http) { }
 
-  private serverApi = 'http://localhost:3000/investor';
+  private serverApi = 'http://localhost:3000/';
 
   public inviteInvestor(investor: Investor) {
-    let URI = this.serverApi + "/inviteinvestor";
+    let URI = this.serverApi + "investor/inviteinvestor";
     let headers = new Headers;
     let body = JSON.stringify({
       _id: 0,
@@ -28,12 +29,13 @@ export class InviteInvestorService {
       wholesaler_id: investor.wholesaler_id
     });
     headers.append('Content-Type', 'application/json');
-    return this.http.post(URI, body, {headers: headers})
+    return this.http.post(URI, body, { headers: headers })
       .map((response) => {
-        return response.json()
+        return response.json();
       })
       .catch((error) => {
         return Observable.throw(error.json());
       })
   }
+
 }
