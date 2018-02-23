8d35a988790b8c336a6af2ec2c15f30571169b5a
Spencer <spencer0cornelia@gmail.com>
2018-02-22 17:47:14 -0800

    refactored addWholesalerConnection to return a promise

--- models/investor.js
+++ models/investor.js
@@ -47,63 +47,73 @@ const InvestorSchema = mongoose.Schema({
 
 const Investor = module.exports = db.model('Investor', InvestorSchema);
 
-module.exports.registerInvestor = function(investor, callback) {
-  Investor.findOne({ 'email': investor.email }, (error, user) => {
-    if (error) {
-      callback(true, {
-        success: false,
-        message: 'Error registering investor.',
-        error: error
-      });
-    } else if (user) {
-      callback(true, {
-        success: false,
-        message: 'Investor already exists. If you are attempting to register a new investor, please head to login page. '
-         + 'If you are attempting to invite an investor, please connect with the investor using our connection process.'
-      });
-    } else {
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(investor.password, salt, (error, hash) => {
-          if (error) {
-            callback(true, {
-              success: false,
-              message: 'Error registering investor.',
-              error: error
-            });
-          }
-          let newInvestor = new Investor({
-            userType: "Investor",
-            userName: investor.userName,
-            password: hash,
-            firstName: investor.firstName,
-            lastName: investor.lastName,
-            email: investor.email,
-            phoneNumber: investor.phoneNumber
-          });
-
-          newInvestor.save((error, i) => {
+module.exports.registerInvestor = function(investor) {
+  return new Promise((resolve, reject) => {
+    Investor.findOne({ 'email': investor.email }, (error, user) => {
+      if (error) {
+        let errorObj = {
+          success: false,
+          message: 'Error registering investor.',
+          error: error
+        }
+        reject(errorObj);
+      } else if (user) {
+        let userObj = {
+          success: false,
+          message: 'Investor already exists. If you are attempting to register a new investor, please head to login page. '
+            + 'If you are attempting to invite an investor, please connect with the investor using our connection process.',
+          error: ''
+        }
+        reject(userObj)
+      } else {
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(investor.password, salt, (error, hash) => {
             if (error) {
-              callback(true, {
+              let errorObj = {
                 success: false,
-                message: 'Error registering investor.'
-              });
-            } else if (i) {
-              callback(false, {
-                success: true,
-                message: 'Successfully registered investor.',
-                data: i
-              });
-            } else {
-              callback(true, {
-                success: false,
-                message: 'Unable to save investor.',
-                error: ''
-              });
+                message: 'Error registering investor.',
+                error: error
+              }
+              reject(errorObj);
             }
-          });
-        })
-      });
-    }
+            let newInvestor = new Investor({
+              userType: "Investor",
+              userName: investor.userName,
+              password: hash,
+              firstName: investor.firstName,
+              lastName: investor.lastName,
+              email: investor.email,
+              phoneNumber: investor.phoneNumber
+            });
+
+            newInvestor.save((error, i) => {
+              if (error) {
+                let errorObj = {
+                  success: false,
+                  message: 'Error registering investor.',
+                  error: error
+                }
+                reject(errorObj);
+              } else if (i) {
+                let investorObj = {
+                  success: true,
+                  message: 'Successfully registered investor.',
+                  data: i
+                }
+                resolve(investorObj);
+              } else {
+                let errorObj = {
+                  success: false,
+                  message: 'Unable to save investor.',
+                  error: ''
+                }
+                reject(errorObj);
+              }
+            });
+          })
+        });
+      }
+    });
   });
 };
 
@@ -201,31 +211,36 @@ module.exports.comparePassword = function(attemptedPassword, investorPassword, c
   });
 }
 
-module.exports.addWholesalerConnection = function(wholesaler, investorID, callback) {
-  Investor.findOneAndUpdate(
-    { '_id': investorID },
-    { $push: { wholesalers: wholesaler } },
-    { safe: true, upsert: true, new: true },
-    function(error, i) {
-      if (error) {
-        callback(true, {
-          success: false,
-          message: 'Error updating investor.',
-          error: error
-        });
-      } else if (i) {
-        callback(false, {
-          success: true,
-          message: 'Successfully updated investor.',
-          data: i
-        })
-      } else {
-        callback(true, {
-          success: false,
-          message: 'Unable to update investor.',
-          error: ''
-        });
+module.exports.addWholesalerConnection = function(wholesaler, investorEmail, callback) {
+  return new Promise((resolve, reject) => {
+    Investor.findOneAndUpdate(
+      { 'email': investorEmail },
+      { $push: { wholesalers: wholesaler } },
+      { safe: true, upsert: true, new: true },
+      function(error, i) {
+        if (error) {
+          let errorObj = {
+            success: false,
+            message: 'Error updating investor.',
+            error: error
+          }
+          reject(errorObj);
+        } else if (i) {
+          let successObj = {
+            success: true,
+            message: 'Successfully updated investor.',
+            data: i
+          }
+          resolve(successObj);
+        } else {
+          let errorObj = {
+            success: false,
+            message: 'Unable to update investor.',
+            error: ''
+          }
+          reject(errorObj);
+        }
       }
-    }
-  );
+    );
+  });
 }
