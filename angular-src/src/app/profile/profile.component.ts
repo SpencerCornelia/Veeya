d423491e8f0cd2003f38d5b41c090ac77fd0bd73
Spencer <spencer0cornelia@gmail.com>
2018-02-24 23:59:32 -0800

    added sidebar to html and added functions for getting necessary profile info on init

--- angular-src/src/app/profile/profile.component.ts
+++ angular-src/src/app/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Property } from '../models/Property';
-import { GetWholesalerPropertiesService } from '../services/getWholesalerProperties.service';
+import { ProfileService } from '../services/profile.service';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 
 @Component({
@@ -16,12 +17,55 @@ export class ProfileComponent implements OnInit {
   private properties: Property[] = [];
   private userID: String;
   private userType: String;
+  private user: any;
 
-  constructor(private getWholesalerPropertiesService: GetWholesalerPropertiesService) { }
+  constructor(private profileService: ProfileService,
+              private flashMessageService: FlashMessagesService) { }
 
   ngOnInit() {
+    this.user = {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      email: '',
+      phoneNumber: '',
+      properties: this.properties
+    }
     this.userID = localStorage.getItem('user_id');
     this.userType = localStorage.getItem('user_type');
+    if (this.userType === 'Wholesaler') {
+      this.getWholesalerProfileInfo();
+    } else if(this.userType === 'Investor') {
+      this.getInvestorProfileInfo();
+    } else {
+      console.log("this.userType is not available.");
+    }
+  }
+
+  getWholesalerProfileInfo() {
+    this.profileService.getWholesalerProfileInfo(this.userID)
+      .subscribe((response) => {
+        this.user = response;
+        this.properties = response.properties;
+      }, (error) => {
+        this.flashMessageService.show(error.message, {
+          cssClass: 'alert-danger',
+          timeout: 3000
+        })
+      })
+  }
+
+  getInvestorProfileInfo() {
+    this.profileService.getInvestorProfileInfo(this.userID)
+      .subscribe((response) => {
+        this.user = response;
+        this.properties = response.properties;
+      }, (error) => {
+        this.flashMessageService.show(error.message, {
+          cssClass: 'alert-danger',
+          timeout: 3000
+        })
+      })
   }
 
 }
