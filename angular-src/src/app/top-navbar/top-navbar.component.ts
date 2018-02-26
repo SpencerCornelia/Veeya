56b907572415a8cb65bb21caba978579c42304bd
Spencer <spencer0cornelia@gmail.com>
2018-02-25 23:24:53 -0800

    added. basic layout is set for now. home, about, contact, logout are current options

---
+++ angular-src/src/app/top-navbar/top-navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-top-navbar',
+  templateUrl: './top-navbar.component.html',
+  styleUrls: ['./top-navbar.component.css']
+})
+export class TopNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
