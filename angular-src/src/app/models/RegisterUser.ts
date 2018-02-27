1b838ccb4e0a78876234c542828db8fdfcd8f210
Spencer <spencer0cornelia@gmail.com>
2018-02-26 21:29:13 -0800

    refactored angular models to represent only having one model: User

---
+++ angular-src/src/app/models/RegisterUser.ts
@@ -0,0 +1,10 @@
+export interface RegisterUser {
+  userType: String,
+  userName: String,
+  password: String,
+  passwordConfirm: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  phoneNumber: String
+}
---
+++ angular-src/src/app/models/User.ts
@@ -0,0 +1,18 @@
+export interface User {
+  _id?: Number,
+  userType: String,
+  firstName: String,
+  lastName: String,
+  password: String,
+  userName: String,
+  email: String,
+  phoneNumber: String,
+  investor_id?: String,
+  wholesaler_id?: String,
+  properties?: Array<any>,
+  minimumLoanAmount?: String,
+  maximumLoanAmount?: String,
+  connections?: Array<any>,
+  terms?: Array<any>,
+  profilePhoto?: String
+}
