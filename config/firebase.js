3a9f7a3019375bb5ebfaf9f0ede3fa5d2173a5e2
Spencer <spencer0cornelia@gmail.com>
2018-03-01 23:29:40 -0800

    adding firebase json credentials

---
+++ config/firebase.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "veeya-c0185",
+  "private_key_id": "33dba21b65b936513c2c7b2d97f7748deed92c14",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDKteKG+iUB/ULO\n5c0fq+on9vsoVbpMvEJsDzUv8lskyTJLfckWDxDy125VvyXRxG8OidbotyrZdtoi\n1MoYJ+hmDkoFFghdUjJmP+hpQ1BGFgmzrQuawviEKn93fy7wxZjAbZfL16d4Or4t\njXrnkD4omnj3LE9kOe0SdA2RUnozaxx8TxBS/I+CwzB0eNRrPPlFzESKQ6IYU99Y\nnCGtWRdVf9ZY3ala5nAou4S62KPNDf3JAHZiztYob+iTziAanh5N+DmnR6V3Zh5L\nutdNqBFsKZ8rLGvnkL4zNvqqnbqH172S8o0r7aHbrBjo7csAIYT7qwgkjQiGRr1E\nVEoWCFCBAgMBAAECggEAARPVHvZyuDW8w1JFepleLrVMno+3Sr1s1SiCIrdB6xDJ\ntgAdAtz0icHoFq5KXzjROU1owvoBESclUvssEgazE/wxjwdDGjhIlDJ03XH4gE+q\n+giqfHmN7hy2t2+JrZYM5Frz+PPav0dN2vqhSfYb90XI7LfQbTebAVanrgHQ+Sri\nZ7dcHYXia/jtSjg/55nx3DgPc3UVu8YJHDFbUlEVWt6qk9XG0p4OcDRnPqTJaWH/\n4KqjEnufOcM2E0oL8jXBEZy98pkJWCRa0ZE9tOTSRuQdKxMbqEa9a//S9+vYGdpR\n+TVl0nrU+leyTE6Uv5BiKabogeDYHUvzkuFMBhvEUQKBgQD1Ct41Bjwhf6+m8g38\nAkzU4bImqpiq7ZwE9iUnYpe2NZk6f+1SfjAfhE8cggAjJ7IdRFcKHYXwNhb+GTzU\nc65PODfqIJgK/NLL9sqpLnrS3y35mf9vUAsGgjG73pi22x1jaQiNHv+Q1cUyl2Fk\nDO/J+M2SvyiBzs+0LSPT4Srw6QKBgQDTxmuUNidkYUD7Myq1PpvN/oZQpEyAcExs\ndePqQQ0YHFIpkg4rlNF3Eavm/knRsLRsik9L67SglYo8dKwX1lZOLDG77iuowGQY\nz2ap/8a2JTkKSuaPeMTiB3vOp9qFbB2Bt3BaT9bDxI1StnbwfSWonOIeSM3g5Q2s\nbyQKBrhj2QKBgE0wEzbYs/BJbGdGMCmF//W+8oNDkbumBVN6xdFWOTvoPlAzI+qs\nXTRQVStBuBFa27xkXpaNHjfAkhWQlZzGo83Ig7R4d+xeH3ioUMZiIcZzDuRlIhVK\nPW7bbBcl0y0mcGd0GoF5lj9lVyzMip0dhhs1CGyFUpkxMDQtU+/WKdShAoGAMEBB\nOp+sEf7mo1VhIbKGJUUcrTsCAzGS+BNUPodbyGaLgdU5TUy2/P4VwKenghNUyHYq\n4w1tNjlcKbIbk8LOHzmKi1sUr3pGVDDNH2abi9ynRmUSaxYFJcq2trsf2s5rWqbE\n/o66TBh4aqQQLXMWJXi5PFUyM0988hDQkUg+suECgYEAssZdeAaArNVjzblGsWQq\nA6HK0/rkvnLiFHwD16v463mjHy40UHu/i9tyiTmV/PoMY0drOWjZ52BJ/6k4pxG3\nHrZK5QwVgGMOZ/Y5Zq6hss5moGC80dMOYxhjjWCnGIE+OeYcxjc8F5cePeyUwF+o\ng9X/auYeYk9dF0BS5EOk3iw=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-99noq@veeya-c0185.iam.gserviceaccount.com",
+  "client_id": "111785022073247901742",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-99noq%40veeya-c0185.iam.gserviceaccount.com"
+}
