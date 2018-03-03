e8a5d5238f0517ea2099aff5121a2a0e17d41a9a
Spencer <spencer0cornelia@gmail.com>
2018-03-02 19:10:42 -0800

    added firebase variables

--- angular-src/src/environments/environment.ts
+++ angular-src/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9uOeXPL1vBe4oWRTe824d4oOq0RcjtfY",
+    authDomain: "veeya-c0185.firebaseapp.com",
+    databaseURL: "https://veeya-c0185.firebaseio.com",
+    projectId: "veeya-c0185",
+    storageBucket: "veeya-c0185.appspot.com",
+    messagingSenderId: "364065119012"
+  }
 };
