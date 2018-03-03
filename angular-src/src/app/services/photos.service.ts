4c0e09edbb1080ddbb7d7f3b260d44214fe88a84
Spencer <spencer0cornelia@gmail.com>
2018-03-02 19:11:39 -0800

    implemented firebase. uploadPhotos successfully adds photos to firebase

--- angular-src/src/app/services/photos.service.ts
+++ angular-src/src/app/services/photos.service.ts
@@ -1,27 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+
+import { AngularFireStorage } from 'angularfire2/storage';
+import * as firebase from 'firebase';
+
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PhotosService {
 
-  constructor(private http: Http) { }
+  private error: Boolean = false;
+  private formData: FormData = new FormData();
+  private photos: Array<File> = [];
+  private photoURLs: Array<String> = [];
+  private propertyPhotosFolder: any;
+  private user_id: String;
 
-  private serverApi = 'http://localhost:3000/photos/';
+  constructor(private storage: AngularFireStorage, private flashMessages: FlashMessagesService) {
+    this.propertyPhotosFolder = 'property-photos';
+    this.user_id = localStorage.getItem('user_id');
+  }
 
-  public uploadPhotos(photos: Array<any>) {
-    let URI = this.serverApi + "/upload";
-    let headers = new Headers;
-    let body = photos;
-    headers.append('Content-Type', 'application/json');
-    return this.http.post(URI, body, {headers: headers})
-      .map((response) => {
-        return response.json();
-      })
-      .catch((error) => {
-        return Observable.throw(error.json());
-      });
+  public uploadPhotos(photos: Array<File>, callback) {
+
+    let storageRef = firebase.storage().ref();
+    for (let i = 0; i < photos.length; i++) {
+      let path = `${this.propertyPhotosFolder}/${this.user_id}/` + photos[i].name;
+      let imageRef = storageRef.child(path);
+      imageRef.put(photos[i])
+        .then((snapshot) => {
+          if (snapshot.state !== 'success') {
+            this.error = true;
+          } else {
+            this.photoURLs.push(path);
+          }
+        })
+    }
+    if (!this.error) {
+      callback(false, this.photoURLs);
+    } else {
+      callback(true);
+    }
   }
 }
