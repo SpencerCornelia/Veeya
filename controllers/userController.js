10532295a681c24725afb3d27a30a20ffcad4add
Spencer <spencer0cornelia@gmail.com>
2018-03-03 21:52:02 -0800

    routing for /user

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const user = require('../models/user');
+
+router.get('/all', (req, res) => {
+  user.getAllUsers()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    })
+});
+
+router.get('/connections/:uid', (req,res) => {
+  let user_id = req.params.uid;
+  user.getAllConnections(user_id)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    })
+});
+
+
+module.exports = router;
