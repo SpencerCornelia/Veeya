3e9d6a0babe684d643a5a1aa154a0b7bcd4f2979
Spencer <spencer0cornelia@gmail.com>
2018-03-03 21:52:48 -0800

    added. functions for getting all users and getting connections for a specific user

---
+++ angular-src/src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { User } from '../models/User';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: Http) { }
+
+  private serverApi = 'http://localhost:3000';
+
+  public getAllUsers():Observable<User[]> {
+    let URI = this.serverApi + "/user/all";
+    return this.http.get(URI)
+      .map(res => res.json())
+      .map(res => <User[]>res.data);
+  }
+
+}
