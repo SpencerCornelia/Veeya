150ae4549dff8e8176245be0561ea3b0b2d0f65a
Spencer <spencer0cornelia@gmail.com>
2018-03-03 18:59:10 -0800

    added connections routing

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -4,20 +4,23 @@ import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
 
 // Add in components
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ViewPropertiesComponent } from './view-properties/view-properties.component';
 import { AddPropertyComponent } from './add-property/add-property.component';
-import { EditPropertyComponent } from './edit-property/edit-property.component'
+import { ConnectionsComponent } from './connections/connections.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditPropertyComponent } from './edit-property/edit-property.component';
 import { InviteInvestorComponent } from './invite-investor/invite-investor.component';
 import { InviteWholesalerComponent } from './invite-wholesaler/invite-wholesaler.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewPropertiesComponent } from './view-properties/view-properties.component';
+
 
 const APP_ROUTES: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'connections', component: ConnectionsComponent, canActivate:[AuthGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate:[AuthGuard] },
   { path: 'properties', component: ViewPropertiesComponent, canActivate:[RoleGuard], data:{userType: 'Wholesaler'} },
