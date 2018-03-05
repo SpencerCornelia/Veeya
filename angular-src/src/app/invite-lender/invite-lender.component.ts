933305f5ba231a51bf91835cb024e060c511e92a
Spencer <spencer0cornelia@gmail.com>
2018-03-04 21:39:59 -0800

    added onSubmit functionality for calling service when form is submitted

--- angular-src/src/app/invite-lender/invite-lender.component.ts
+++ angular-src/src/app/invite-lender/invite-lender.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { User } from '../models/User';
+import { AppRoutingModule } from '../app-routing.module';
+import { Router } from '@angular/router';
+import { InviteService } from '../services/invite.service';
+import { ModuleWithProviders } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-invite-lender',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InviteLenderComponent implements OnInit {
 
-  constructor() { }
+  private lender: User;
+
+  constructor(private inviteService: InviteService,
+              private router: Router,
+              private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+    let user_id = localStorage.getItem('user_id');
+    this.lender = {
+      userType: 'Lender',
+      userName: '',
+      password: 'initialPassword',
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+      user_id: user_id
+    }
+  }
+
+  public onSubmit() {
+    this.lender.userName = this.lender.firstName.toString() + this.lender.lastName.toString();
+    this.inviteService.inviteLender(this.lender)
+      .subscribe((response) => {
+        this.router.navigate(['/dashboard']);
+        this.flashMessage.show(response.message, {
+          cssClass: 'alert-success',
+          timeout: 3000
+        });
+      },
+      (error) => {
+        this.flashMessage.show(error.message, {
+          cssClass: 'alert-danger',
+          timeout: 3000
+        });
+      });
   }
 
 }
