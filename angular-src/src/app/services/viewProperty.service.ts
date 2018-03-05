b1c72a518bbe44ed4342d4b6b6d1f9ebf5969931
Spencer <spencer0cornelia@gmail.com>
2018-03-04 17:44:29 -0800

    added for viewing single property

---
+++ angular-src/src/app/services/viewProperty.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Property } from '../models/Property';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ViewPropertyService {
+
+  constructor(private http: Http) { }
+
+  private serverApi = 'http://localhost:3000/properties';
+
+  public getPropertyById(propertyId: String):Observable<Property> {
+    let URI = this.serverApi + "/property/" + propertyId;
+    return this.http.get(URI)
+      .map(res => res.json())
+      .map(res => <Property>res.data);
+  }
+
+}
