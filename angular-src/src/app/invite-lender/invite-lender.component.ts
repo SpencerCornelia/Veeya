d5dd7f089a3fd965a3079a45350e5c249cc99553
Spencer <spencer0cornelia@gmail.com>
2018-03-04 20:52:44 -0800

    added

---
+++ angular-src/src/app/invite-lender/invite-lender.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-lender',
+  templateUrl: './invite-lender.component.html',
+  styleUrls: ['./invite-lender.component.css']
+})
+export class InviteLenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
