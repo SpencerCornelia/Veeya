bcdeeb8784e48ce682d33ff78cadf6850a6ef1f8
Spencer <spencer0cornelia@gmail.com>
2018-03-04 17:52:30 -0800

    added endpoints for retrieving all wholesalers && all investors

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,26 @@ router.get('/all', (req, res) => {
     })
 });
 
+router.get('/all/investors', (req, res) => {
+  user.getAllInvestors()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/all/wholesalers', (req, res) => {
+  user.getAllWholesalers()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
 router.get('/connections/:uid', (req,res) => {
   let user_id = req.params.uid;
   user.getAllConnections(user_id)
