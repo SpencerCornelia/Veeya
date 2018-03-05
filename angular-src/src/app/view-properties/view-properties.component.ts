b0be4b60b6a1b08711961e29a9a7bafb433b24e2
Spencer <spencer0cornelia@gmail.com>
2018-03-04 20:47:51 -0800

    commented code. html will be replaced by template

--- angular-src/src/app/view-properties/view-properties.component.ts
+++ angular-src/src/app/view-properties/view-properties.component.ts
@@ -15,21 +15,57 @@ import { Router } from '@angular/router';
 })
 export class ViewPropertiesComponent implements OnInit {
 
-  // array of Property type
   private properties: Property[] = [];
-  private wholesalerProperties: Property[] = [];
+
+  private wholesalerPropertiesListed: Property[] = [];
+  private wholesalerPropertiesSold: Property[] = [];
+
+  private investorPropertiesBought: Property[] = [];
+  private investorPropertiesConnected: Property[] = [];
+  private investorPropertiesStarred: Property[] = [];
 
   constructor(private getPropertyService: GetAllPropertiesService, private deletePropertyService: DeletePropertyService, private getUserPropertiesService: GetUserPropertiesService, private editPropertyService: EditPropertyService, private router: Router) { }
 
   ngOnInit() {
-    this.getPropertiesForWholesaler();
+    let userType = localStorage.getItem('user_type');
+    if (userType === 'Wholesaler') {
+      this.getPropertiesForWholesaler();
+
+      this.properties.forEach((property, index) => {
+        if (property.status === 'Listed') {
+          this.wholesalerPropertiesListed.push(property);
+        } else if (property.status === 'Sold') {
+          this.wholesalerPropertiesSold.push(property);
+        }
+      });
+
+    } else if (userType === 'Investor') {
+      this.getPropertiesForInvestor();
+
+      this.properties.forEach((property, index) => {
+        if (property.status === 'Bought') {
+          this.investorPropertiesBought.push(property);
+        } else if (property.status === 'Connection') {
+          this.investorPropertiesConnected.push(property);
+        } else if (property.status === 'Starred') {
+          this.investorPropertiesStarred.push(property);
+        }
+      });
+
+    }
   }
 
   public getPropertiesForWholesaler() {
     let wholesalerID = localStorage.getItem('user_id');
     // Get all properties for the wholesaler who is logged in
-    this.getUserPropertiesService.getUserProperties(wholesalerID)
-      .subscribe(response => this.wholesalerProperties = response)
+    this.getUserPropertiesService.getWholesalerUserProperties(wholesalerID)
+      .subscribe(response => this.properties = response)
+  }
+
+  public getPropertiesForInvestor() {
+    let investorID = localStorage.getItem('user_id');
+    this.getUserPropertiesService.getInvestorUserProperties(investorID)
+      .subscribe(response => this.properties = response)
   }
 
   public loadAllProperties() {
@@ -41,14 +77,6 @@ export class ViewPropertiesComponent implements OnInit {
   public deleteProperty(property: Property) {
     this.deletePropertyService.deleteProperty(property._id)
       .subscribe(response => this.properties = this.properties.filter(properties => properties !== property))
-  }
-
-  public onAddProperty(newProperty) {
-    this.properties = this.properties.concat(newProperty);
-  }
-
-  public editProperty(property: Property) {
-    this.router.navigate(['/properties/editproperty/' + property._id]);
   }
 
 }
