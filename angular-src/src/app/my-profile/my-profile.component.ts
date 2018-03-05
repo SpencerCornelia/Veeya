2ad6bb2ddfb6339d3bb3dc03c649682a11bcb2d5
Spencer <spencer0cornelia@gmail.com>
2018-03-04 20:46:48 -0800

    added

---
+++ angular-src/src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../models/User';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent implements OnInit {
+
+  private user: User;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    let user_id = localStorage.getItem('user_id');
+    this.getUser(user_id);
+
+    this.user = {
+      userType: '',
+      userName: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      phoneNumber: ''
+    }
+  }
+
+  getUser(user_id) {
+    this.userService.getUserById(user_id)
+      .subscribe((response) => {
+        this.user = response;
+      }, (error) => {
+
+      })
+
+  }
+
+}
