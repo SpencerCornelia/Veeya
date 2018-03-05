abd919ff93307ca3851600747e1b0f711f9c7238
Spencer <spencer0cornelia@gmail.com>
2018-03-04 16:42:58 -0800

    added

---
+++ angular-src/src/app/view-property/view-property.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-property',
+  templateUrl: './view-property.component.html',
+  styleUrls: ['./view-property.component.css']
+})
+export class ViewPropertyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
