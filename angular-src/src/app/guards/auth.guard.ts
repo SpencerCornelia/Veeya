c7a94a1ad164f3a44162825175b8d5cbf2e42fd0
Spencer <spencer0cornelia@gmail.com>
2018-03-07 20:04:13 -0800

    added functionality for redirecting url if user isn't logged in

--- angular-src/src/app/guards/auth.guard.ts
+++ angular-src/src/app/guards/auth.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    let url: String = state.url;
+
+    return this.checkLogin(url);
+  }
+
+  checkLogin(url) {
     if (this.authService.loggedIn()) {
       return true;
     } else {
+      this.authService.redirectUrl = url;
       this.router.navigate(['/login']);
       return false;
     }
