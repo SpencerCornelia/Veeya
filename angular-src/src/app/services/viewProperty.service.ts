cc7a1120ebc6cd98c00b5862413221903535ddb0
Spencer <spencer0cornelia@gmail.com>
2018-03-16 16:47:45 -0700

    removed behavior subject behavior

--- angular-src/src/app/services/viewProperty.service.ts
+++ angular-src/src/app/services/viewProperty.service.ts
@@ -5,15 +5,12 @@ import { Observable } from 'rxjs/Observable';
 import { Property } from '../models/Property';
 
 import 'rxjs/add/operator/map';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class ViewPropertyService {
 
   constructor(private http: Http) { }
 
-  private selectedProperty = new BehaviorSubject<Property>(null);
   private serverApi = 'http://localhost:3000/properties';
 
   public getPropertyById(propertyId: String):Observable<Property> {
@@ -21,14 +18,6 @@ export class ViewPropertyService {
     return this.http.get(URI)
       .map(res => res.json())
       .map(res => <Property>res.data);
-  }
-
-  public setProperty(property: Property) {
-    this.selectedProperty.next(property);
-  }
-
-  public getProperty(): Observable<Property> {
-    return this.selectedProperty.asObservable();
   }
 
 }
