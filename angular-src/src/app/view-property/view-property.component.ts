c5d5b59d4043e6bd41d7ce18ee960a83b2554e91
Spencer <spencer0cornelia@gmail.com>
2018-03-15 21:15:52 -0700

    data is rendered successfully. receive data from service instead of making api calls

--- angular-src/src/app/view-property/view-property.component.ts
+++ angular-src/src/app/view-property/view-property.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Property } from '../models/Property';
+import { Component, OnInit, Input } from '@angular/core';
 import { AppRoutingModule } from '../app-routing.module';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { Property } from '../models/Property';
 import { User } from '../models/User';
 
 import { AuthService } from '../services/auth.service';
 import { EditPropertyService } from '../services/editProperty.service';
 import { PhotosService } from '../services/photos.service';
+import { TogglePropertyService } from '../services/toggleProperty.service';
 import { ValidateService } from '../services/validate.service';
 import { ViewPropertyService } from '../services/viewProperty.service';
 
@@ -20,88 +21,65 @@ import { ViewPropertyService } from '../services/viewProperty.service';
 export class ViewPropertyComponent implements OnInit {
 
   private currentUser: User;
-  private pageTitle: String;
   private photo: File;
-  private testPhotos: Array<String> = [];
-  private photos: Array<String> = [];
   private propertyID: string;
-  private initialProperty: Property;
+  private propertyOwner: Boolean;
+  private property: Property;
 
   constructor(private route: ActivatedRoute,
               private authService: AuthService,
               private viewPropertyService: ViewPropertyService,
               private router: Router,
               private photosService: PhotosService,
+              private togglePropertyService: TogglePropertyService,
               private validateService: ValidateService) {
     this.getCurrentUser();
     this.propertyID = route.snapshot.params['id'];
-    // this.getProperty(this.propertyID);
+    this.getProperty();
   }
 
   ngOnInit() {
-    this.viewPropertyService.getProperty()
-      .subscribe((property) => {
-        this.initialProperty = property;
-      }, (error) => {
-        console.log("error:", error);
-      })
+    this.propertyOwner = this.confirmPropertyOwnership();
+    let wholesalerID = this.authService.loggedInUser();
 
-    // this.initialProperty = {
-    //   _id: 0,
-    //   wholesaler_id: '',
-    //   address: '',
-    //   city: '',
-    //   state: 'AL',
-    //   zipCode: '',
-    //   purchasePrice: '',
-    //   bedrooms: 0,
-    //   bathrooms: 0,
-    //   expectedRehab: '',
-    //   HOA: '',
-    //   propertyTaxes: '',
-    //   utilities: '',
-    //   afterRepairValue: '',
-    //   capRate: '',
-    //   averageRent: '',
-    //   squareFootage: '',
-    //   insurance: '',
-    //   propertyType: 'Single Family',
-    //   yearBuilt: '',
-    //   status: 'Listed',
-    //   sellerFinancing: 'false',
-    //   comps: [
-    //     {
-    //       firstCompAddress: '',
-    //       firstCompPrice: ''
-    //     },
-    //     {
-    //       secondCompAddress: '',
-    //       secondCompPrice: ''
-    //     },
-    //     {
-    //       thirdCompAddress: '',
-    //       thirdCompPrice: ''
-    //     }
-    //   ],
-    //   photos: []
-    // }
-
-    this.currentUser = {
-      userType: '',
-      userName: '',
-      firstName: '',
-      lastName: '',
-      password: '',
-      email: '',
-      phoneNumber: '',
+    this.property = {
+      _id: 0,
+      wholesaler_id: wholesalerID,
+      address: '',
       city: '',
-      state: '',
-      URLs: {
-        personal: '',
-        facebook: '',
-        linkedIn: '',
-        biggerPockets: ''
-      }
+      state: 'AL',
+      zipCode: '',
+      purchasePrice: '',
+      bedrooms: 0,
+      bathrooms: 0,
+      expectedRehab: '',
+      HOA: '',
+      propertyTaxes: '',
+      utilities: '',
+      afterRepairValue: '',
+      capRate: '',
+      averageRent: '',
+      squareFootage: '',
+      insurance: '',
+      propertyType: 'Single Family',
+      yearBuilt: '',
+      status: 'Listed',
+      sellerFinancing: 'false',
+      comps: [
+        {
+          firstCompAddress: '',
+          firstCompPrice: ''
+        },
+        {
+          secondCompAddress: '',
+          secondCompPrice: ''
+        },
+        {
+          thirdCompAddress: '',
+          thirdCompPrice: ''
+        }
+      ],
+      photos: []
     }
   }
 
@@ -114,14 +92,17 @@ export class ViewPropertyComponent implements OnInit {
       })
   }
 
-  getProperty(id) {
-    this.viewPropertyService.getPropertyById(id)
+  getProperty() {
+    this.viewPropertyService.getProperty()
       .subscribe((response) => {
-        this.initialProperty = response;
-        this.photos = this.initialProperty.photos;
+        this.property = response;
       }, (error) => {
 
       });
+  }
+
+  confirmPropertyOwnership() {
+    return this.property.wholesaler_id === this.authService.loggedInUser() ? true : false;
   }
 }
 
