0ab62077d761e2fef8522c36bd861b0e7dfe5768
Spencer <spencer0cornelia@gmail.com>
2018-03-15 21:17:03 -0700

    getProperty working properly

--- angular-src/src/app/services/viewProperty.service.ts
+++ angular-src/src/app/services/viewProperty.service.ts
@@ -5,12 +5,15 @@ import { Observable } from 'rxjs/Observable';
 import { Property } from '../models/Property';
 
 import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class ViewPropertyService {
 
   constructor(private http: Http) { }
 
+  private selectedProperty = new ReplaySubject<Property>(1);
   private serverApi = 'http://localhost:3000/properties';
 
   public getPropertyById(propertyId: String):Observable<Property> {
@@ -18,6 +21,14 @@ export class ViewPropertyService {
     return this.http.get(URI)
       .map(res => res.json())
       .map(res => <Property>res.data);
+  }
+
+  public setProperty(property: Property) {
+    this.selectedProperty.next(property);
+  }
+
+  public getProperty(): Observable<Property> {
+    return this.selectedProperty.asObservable();
   }
 
 }
