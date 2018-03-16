0d1d8e029a2926c26aa75373afc0fe13a9c66034
Spencer <spencer0cornelia@gmail.com>
2018-03-15 23:20:57 -0700

    added logic for edit, listed, sold, delete, cancel buttons at bottom of form

--- angular-src/src/app/view-property/view-property.component.ts
+++ angular-src/src/app/view-property/view-property.component.ts
@@ -6,6 +6,7 @@ import { Property } from '../models/Property';
 import { User } from '../models/User';
 
 import { AuthService } from '../services/auth.service';
+import { DeletePropertyService } from '../services/deleteProperty.service';
 import { EditPropertyService } from '../services/editProperty.service';
 import { PhotosService } from '../services/photos.service';
 import { TogglePropertyService } from '../services/toggleProperty.service';
@@ -21,13 +22,18 @@ import { ViewPropertyService } from '../services/viewProperty.service';
 export class ViewPropertyComponent implements OnInit {
 
   private currentUser: User;
+  private editMode: Boolean = false;
   private photo: File;
+  private photos: Array<string>;
+  private photosToAdd: Array<File> = [];
   private propertyID: string;
   private propertyOwner: Boolean;
   private property: Property;
 
   constructor(private route: ActivatedRoute,
               private authService: AuthService,
+              private deletePropertyService: DeletePropertyService,
+              private editPropertyService: EditPropertyService,
               private viewPropertyService: ViewPropertyService,
               private router: Router,
               private photosService: PhotosService,
@@ -39,7 +45,6 @@ export class ViewPropertyComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.propertyOwner = this.confirmPropertyOwnership();
     let wholesalerID = this.authService.loggedInUser();
 
     this.property = {
@@ -81,6 +86,24 @@ export class ViewPropertyComponent implements OnInit {
       ],
       photos: []
     }
+
+    this.currentUser = {
+      userType: '',
+      firstName: '',
+      lastName: '',
+      userName: '',
+      password: '',
+      email: '',
+      phoneNumber: '',
+      city: '',
+      state: '',
+      URLs: {
+        personal: '',
+        facebook: '',
+        linkedIn: '',
+        biggerPockets: ''
+      }
+    }
   }
 
   getCurrentUser() {
@@ -96,6 +119,7 @@ export class ViewPropertyComponent implements OnInit {
     this.viewPropertyService.getProperty()
       .subscribe((response) => {
         this.property = response;
+        this.propertyOwner = this.confirmPropertyOwnership();
       }, (error) => {
 
       });
@@ -103,6 +127,115 @@ export class ViewPropertyComponent implements OnInit {
 
   confirmPropertyOwnership() {
     return this.property.wholesaler_id === this.authService.loggedInUser() ? true : false;
+  }
+
+  addPhoto(event) {
+    let file = event.target.files[0];
+    let fileType = file["type"];
+    if (this.validateService.validatePhotoInput(fileType)) {
+      this.photo = event.target.files[0];
+      this.photosToAdd.push(this.photo);
+      document.getElementById('selectedFiles').innerHTML += file.name + "</br>";
+
+      if (this.photos.length + this.photosToAdd.length === 3) {
+        let inputButton = (<HTMLInputElement>document.getElementById('imageInput'));
+        inputButton.disabled = true;
+      }
+    }
+
+  }
+
+  uploadPhotos(event) {
+    document.getElementById('uploadPhotos').setAttribute('disabled', 'disabled');
+    this.photosService.uploadPropertyPhotos(this.photosToAdd, (error, photos) => {
+      if (error) {
+
+      } else {
+        let inputValue = (<HTMLInputElement>document.getElementById('imageInput'));
+        inputValue.value = "";
+        document.getElementById('removePhotos').hidden = true;
+        document.getElementById('uploadPhotos').hidden = true;
+        this.photos = photos;
+        this.photosToAdd = [];
+      }
+    })
+  }
+
+  removePhoto(photo) {
+    this.photosService.removePropertyPhoto(photo, (error) => {
+      if (error) {
+        // error message = 'Error removing photo. Please try again.'
+      } else {
+        // message = 'Successfully removed photo.'
+      }
+    });
+  }
+
+  onSubmit() {
+    this.photosService.getPropertyPhotoUrls(this.photos, (error, photos) => {
+      if (error) {
+        // error message
+        return;
+      } else {
+        this.property.photos = photos;
+      }
+    });
+
+    this.editPropertyService.editProperty(this.property)
+      .subscribe((response) => {
+        if (response.success) {
+          this.router.navigate(['/dashboard']);
+       }
+      }, (error) => {
+
+      });
+  }
+
+  edit() {
+    this.editMode = true;
+  }
+
+  sold() {
+    let soldConfirm = confirm("Are you sure you want to mark this property as sold?");
+    if (soldConfirm) {
+      this.property.status = "Sold";
+      this.editPropertyService.editProperty(this.property)
+        .subscribe((response) => {
+          if (response.success) {
+          }
+        }, (error) => {
+
+        });
+    }
+  }
+
+  listed() {
+    let listedConfirm = confirm("Are you sure you want to mark this property as listed?");
+    if (listedConfirm) {
+      this.property.status = "Listed";
+      this.editPropertyService.editProperty(this.property)
+        .subscribe((response) => {
+          if (response.success) {
+          }
+        }, (error) => {
+
+        });
+    }
+  }
+
+  deleteProperty() {
+    let deleteConfirm = confirm("Are you sure you want to delete this property?");
+    if (deleteConfirm) {
+      this.deletePropertyService.removePhotos(this.property.photos);
+      this.deletePropertyService.deleteProperty(this.property._id)
+        .subscribe((response) => {
+          if (response.success) {
+            this.router.navigate(['/dashboard']);
+          }
+        },(error) => {
+
+        })
+    }
   }
 }
 
