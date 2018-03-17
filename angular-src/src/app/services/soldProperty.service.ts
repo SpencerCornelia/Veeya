74aabf321e9afffb70d04c9492e4836076394120
Spencer <spencer0cornelia@gmail.com>
2018-03-16 21:00:00 -0700

    added for making api call to handle property sold functionality

---
+++ angular-src/src/app/services/soldProperty.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Property } from '../models/Property';
+import { User } from '../models/User';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SoldPropertyService {
+
+  constructor(private http: Http) { }
+
+  private serverApi = 'http://localhost:3000/properties';
+
+  public soldProperty(investor: User, wholesalerID: string, property: Property) {
+    let URI = this.serverApi + "/soldproperty";
+    let headers = new Headers;
+    let body = JSON.stringify({
+      investor: investor,
+      wholesalerId: wholesalerID,
+      property: property
+    });
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(URI, body, {headers: headers})
+      .map((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        return Observable.throw(error.json());
+      });
+  }
+}
