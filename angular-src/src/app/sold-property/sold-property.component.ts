41ecd4734df24cca6f4a36f23056671cb0c361ce
Spencer <spencer0cornelia@gmail.com>
2018-03-16 21:01:05 -0700

    working functionality for selling a property

--- angular-src/src/app/sold-property/sold-property.component.ts
+++ angular-src/src/app/sold-property/sold-property.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+declare var $: any;
 
 import { Property } from '../models/Property';
 import { User } from '../models/User';
 
+import { AuthService } from '../services/auth.service';
+import { SoldPropertyService } from '../services/soldProperty.service';
+import { UserService } from '../services/user.service';
 import { ViewPropertyService } from '../services/viewProperty.service';
 
 @Component({
@@ -13,36 +18,86 @@ import { ViewPropertyService } from '../services/viewProperty.service';
 export class SoldPropertyComponent implements OnInit {
 
   private currentUser: User;
+  private investors: Array<User> = [];
   private property: Property;
+  private propertyId: string;
+  private wholesalerID: string;
 
-  constructor(private viewPropertyService: ViewPropertyService) { }
+  private displayMessage: Boolean = true;
+  private displayBody: Boolean = false;
+
+  private selectedInvestor: User;
+
+  constructor(
+    private authService: AuthService,
+    private soldPropertyService: SoldPropertyService,
+    private viewPropertyService: ViewPropertyService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
 
-    this.currentUser = {
-      userType: '',
-      firstName: '',
-      lastName: '',
-      userName: '',
-      password: '',
-      email: '',
-      phoneNumber: '',
-      city: '',
-      state: '',
-      URLs: {
-        personal: '',
-        facebook: '',
-        linkedIn: '',
-        biggerPockets: ''
-      }
-    }
+    this.wholesalerID = this.authService.loggedInUser();
+    this.propertyId = this.route.snapshot.params.id;
+
+    this.getProperty();
+    this.getInvestors();
   }
 
   getProperty() {
-    this.viewPropertyService.getProperty()
-      .subscribe((property) => {
-        this.property = property;
+    this.viewPropertyService.getSoldProperty()
+      .subscribe((response) => {
+        this.displayMessage = false;
+        this.displayBody = true;
+        this.property = response;
       })
+  }
+
+  getInvestors() {
+    this.userService.getAllInvestors()
+      .subscribe((response) => {
+        this.investors = response;
+      }, (error) => {
+
+      });
+  }
+
+  showModal(investor) {
+    let that = this;
+
+    if (!this.property) {
+      this.viewPropertyService.getPropertyById(this.propertyId)
+        .subscribe((response) => {
+          this.property = response;
+          this.selectedInvestor = investor;
+          $("#soldModal").modal('show');
+          $("#soldConfirm").on('click', function() {
+            that.soldPropertyService.soldProperty(that.selectedInvestor, that.wholesalerID, that.property)
+              .subscribe((response) => {
+                $("#soldModal").modal('hide');
+                that.router.navigate(['/dashboard']);
+              }, (error) => {
+
+              });
+          });
+        }, (error) => {
+
+        });
+    } else {
+      this.selectedInvestor = investor;
+      $("#soldModal").modal('show');
+      $("#soldConfirm").on('click', function() {
+        that.soldPropertyService.soldProperty(that.selectedInvestor, that.wholesalerID, that.property)
+          .subscribe((response) => {
+            $("#soldModal").modal('hide');
+            that.router.navigate(['/dashboard']);
+          }, (error) => {
+
+          });
+      });
+    }
+
   }
 
 }
