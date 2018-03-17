3061dc0d7bdff7369908bb1cdd35fa75d9bbf703
Spencer <spencer0cornelia@gmail.com>
2018-03-17 16:45:41 -0700

    removed investor and wholesaler passport file.  added user passport file

--- config/userPassport.js
+++ config/userPassport.js
@@ -10,7 +10,7 @@ module.exports = function(passport) {
   opts.secretOrKey = keys.secret;
 
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.getInvestorById(jwt_payload._id, (err, user) => {
+    User.getUserById(jwt_payload._id, (err, user) => {
       if (err) {
         return done(err, false);
       }
