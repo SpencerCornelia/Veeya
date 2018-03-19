32c3d7ae50dae0628fd0a1c43854bf3a97522507
Spencer <spencer0cornelia@gmail.com>
2018-03-19 06:03:59 -0700

    added for sending connection request

---
+++ angular-src/src/app/services/addConnection.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { User } from '../models/User';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AddConnectionService {
+
+  constructor(private http: Http) { }
+
+  private serverApi = 'http://localhost:3000';
+
+  public addConnection(userId: string, connectionUserId: string):Observable<User[]> {
+    let URI = this.serverApi + "/user/addconnection";
+    let headers = new Headers;
+    headers.append('Content-Type', 'application/json');
+    let body = JSON.stringify({
+      currentUserId: userId,
+      connectionUserId: connectionUserId
+    });
+    return this.http.post(URI, body, { headers: headers })
+      .map(res => res.json())
+      .map(res => <User[]>res.data);
+  }
+
+}
