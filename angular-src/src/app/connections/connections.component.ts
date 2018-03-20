7b3d2d9822a8674f3532b67405da786cab8c070d
Spencer <spencer0cornelia@gmail.com>
2018-03-19 20:44:15 -0700

    working functionality for sending connection request and accepting it. using logic to display different elements/text on screen based off current user having any pending/current connections or not

--- angular-src/src/app/connections/connections.component.ts
+++ angular-src/src/app/connections/connections.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { User } from '../models/User';
 import { AuthService } from '../services/auth.service';
+import { AddConnectionService } from '../services/addConnection.service';
 import { GetConnectionsService } from '../services/getConnections.service';
 import { UserService } from '../services/user.service';
 
@@ -13,16 +14,22 @@ import { UserService } from '../services/user.service';
 export class ConnectionsComponent implements OnInit {
 
   private connections: Array<User> = [];
+
+  private pendingConnections: Boolean = false;
+
+  private pendingConnectionsArray: Array<User> = [];
   private searchText: String;
   private user_id: String;
 
-  constructor(private authService: AuthService,
+  constructor(private addConnectionService: AddConnectionService,
+              private authService: AuthService,
               private getConnectionsService: GetConnectionsService,
               private userService: UserService) { }
 
   ngOnInit() {
     this.user_id = this.authService.loggedInUser();
     this.getConnectionsForUser();
+    this.getPendingConnections();
   }
 
   getConnectionsForUser() {
@@ -32,6 +39,40 @@ export class ConnectionsComponent implements OnInit {
       }, (error) => {
 
       });
+  }
+
+  getPendingConnections() {
+    this.getConnectionsService.getPendingConnections(this.user_id)
+      .subscribe((response) => {
+        this.pendingConnectionsArray = response;
+        if (this.pendingConnectionsArray.length > 0) {
+          this.pendingConnections = true;
+        }
+      }, (error) => {
+
+      });
+  }
+
+  acceptRequest(connection) {
+    let connectionId = connection._id;
+    this.addConnectionService.acceptConnection(this.user_id, connectionId)
+      .subscribe((response) => {
+        this.connections.push(response.connectionUser);
+        if (this.pendingConnectionsArray.length == 1) {
+          this.pendingConnections = false;
+          this.pendingConnectionsArray = [];
+        } else {
+          this.pendingConnectionsArray.filter((connection) => {
+            return connection._id != response.connectionUser._id;
+          });
+        }
+      }, (error) => {
+
+      })
+  }
+
+  denyRequest() {
+
   }
 
 }
