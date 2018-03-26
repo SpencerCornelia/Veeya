6068aef1e6409a24132bc2a1a7e404895d3e4afa
Spencer <spencer0cornelia@gmail.com>
2018-03-25 21:16:50 -0700

    added this.property from call/subscribe to customizePropertyService

--- angular-src/src/app/generate-report/generate-report.component.ts
+++ angular-src/src/app/generate-report/generate-report.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { CustomizePropertyService } from '../services/customizeProperty.service';
+
+import { Property } from '../models/Property';
 
 @Component({
   selector: 'app-generate-report',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateReportComponent implements OnInit {
 
-  constructor() { }
+  private property: any;
+
+  constructor(private customizePropertyService: CustomizePropertyService) { }
 
   ngOnInit() {
+    if (this.customizePropertyService.customizedPropertyExists) {
+      this.property = this.customizePropertyService.getCustomizedProperty();
+      console.log("this.property EXISTS:", this.property)
+    } else {
+      this.customizePropertyService.getProperty()
+        .subscribe((response) => {
+          console.log("response:", response)
+          this.property = response;
+        })
+    }
   }
 
 }
