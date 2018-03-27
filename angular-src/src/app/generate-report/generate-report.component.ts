aa7470d21ce00accc0013897d0005e8e21051470
Spencer <spencer0cornelia@gmail.com>
2018-03-26 23:44:56 -0700

    added variables for handling previous/next year button functionality

--- angular-src/src/app/generate-report/generate-report.component.ts
+++ angular-src/src/app/generate-report/generate-report.component.ts
@@ -12,13 +12,28 @@ import { Property } from '../models/Property';
 })
 export class GenerateReportComponent implements OnInit {
 
+  private currentMonth: any;
+  private currentYear: any;
+
+  // set for previous year and next year buttons
+  private finalPaymentYear: any;
+  private firstYear: boolean = true;
+  private lastYear: boolean = false;
+
   private property: any;
 
   constructor(private customizePropertyService: CustomizePropertyService) { }
 
   ngOnInit() {
+    var date = new Date();
+    this.currentMonth = date.getMonth();
+    this.currentYear = date.getFullYear();
+    this.firstYear = date.getFullYear() == this.currentYear ? true : false;
+    this.lastYear = this.finalPaymentYear == this.currentYear;
+
     if (this.customizePropertyService.customizedPropertyExists) {
       this.property = this.customizePropertyService.getCustomizedProperty();
+      this.setLastYear();
       console.log("this.property EXISTS:", this.property)
     } else {
       this.customizePropertyService.getProperty()
@@ -26,6 +41,13 @@ export class GenerateReportComponent implements OnInit {
           console.log("response:", response)
           this.property = response;
         })
+    }
+  }
+
+  setLastYear() {
+    this.finalPaymentYear = (this.property.numberOfPayments / 12) + this.currentYear;
+    if (this.currentMonth == 'January') {
+      this.finalPaymentYear = this.finalPaymentYear - 1;
     }
   }
 
