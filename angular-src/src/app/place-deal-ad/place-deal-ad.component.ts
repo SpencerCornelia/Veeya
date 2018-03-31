83d195877ebbf2c2ff281311ff1c3084fb94312b
Spencer <spencer0cornelia@gmail.com>
2018-03-30 20:39:12 -0700

    form is built. button submits and calls service. layout is satisfactory

--- angular-src/src/app/place-deal-ad/place-deal-ad.component.ts
+++ angular-src/src/app/place-deal-ad/place-deal-ad.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { NewAd } from '../models/NewAd';
+import { AuthService } from '../services/auth.service';
+import { DealAdService } from '../services/dealAd.service';
 
 @Component({
   selector: 'app-place-deal-ad',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDealAdComponent implements OnInit {
 
-  constructor() { }
+  private currentUser: string;
+  private newAd: NewAd;
+
+  constructor(private dealAdService: DealAdService,
+              private router: Router
+              private authService: AuthService) { }
 
   ngOnInit() {
+    this.currentUser = this.authService.loggedInUser();
+    this.newAd = {
+      city: '',
+      state: 'AL',
+      zipCode: '',
+      purchasePrice: '',
+      bedrooms: '0',
+      bathrooms: '0',
+      squareFootage: '',
+      yearBuilt: '',
+      sellerFinancing: 'No',
+      propertyType: 'Single Family',
+      netOperatingIncome: '',
+      capRate: '',
+      cashOnCashReturn: '',
+      internalRateOfReturn: ''
+    }
+  }
+
+  placeAd() {
+    let confirmed = window.confirm("Have you entered all valid data?");
+    if (confirmed) {
+      this.dealAdService.placeNewAd(this.newAd, this.currentUser)
+        .subscribe((response) => {
+          if (response.success) {
+            // success message to user
+            this.router.navigate(['/dashboard']);
+          }
+        }, (error) => {
+
+        });
+    }
   }
 
 }
