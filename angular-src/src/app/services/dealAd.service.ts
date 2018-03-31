06e85aa7b92a6389c6e53fcf79c536fe064960d4
Spencer <spencer0cornelia@gmail.com>
2018-03-30 20:38:16 -0700

    added for making API call to place new wanted deal for investor

---
+++ angular-src/src/app/services/dealAd.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { NewAd } from '../models/NewAd';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DealAdService {
+
+  constructor(private http: Http) { }
+
+  private serverApi = 'http://localhost:3000';
+
+  public placeNewAd(newAd: NewAd, investorId: string) {
+    let URI = this.serverApi + "/ads/placeNewAd";
+    let headers = new Headers;
+    headers.append('Content-Type', 'application/json');
+    let body = JSON.stringify({
+      investorId: investorId,
+      newAd: newAd
+    });
+    return this.http.post(URI, body, { headers: headers })
+      .map(res => res.json())
+      .map(res => res);
+  }
+
+}
