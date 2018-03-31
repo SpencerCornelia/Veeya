347a453602528a2a764c31ecb0ac7c36b86d54af
Spencer <spencer0cornelia@gmail.com>
2018-03-30 20:37:58 -0700

    added for handling investor placing a new ad functionality

---
+++ angular-src/src/app/models/NewAd.ts
@@ -0,0 +1,16 @@
+export interface NewAd {
+  city: string,
+  state: string,
+  zipCode: string,
+  propertyType: string,
+  purchasePrice: string,
+  bedrooms: string,
+  bathrooms: string,
+  squareFootage: string,
+  yearBuilt: string,
+  sellerFinancing: string,
+  netOperatingIncome: string,
+  capRate: string,
+  cashOnCashReturn: string,
+  internalRateOfReturn: string
+}
