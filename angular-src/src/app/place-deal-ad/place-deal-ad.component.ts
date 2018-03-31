ac439779d232a73db18b7aac3589cc416a513c76
Spencer <spencer0cornelia@gmail.com>
2018-03-30 17:53:45 -0700

    added

---
+++ angular-src/src/app/place-deal-ad/place-deal-ad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-deal-ad',
+  templateUrl: './place-deal-ad.component.html',
+  styleUrls: ['./place-deal-ad.component.css']
+})
+export class PlaceDealAdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
