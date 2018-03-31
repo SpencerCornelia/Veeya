fa1bed85f0a2cd0038bcf0baaaef68e104df4116
Spencer <spencer0cornelia@gmail.com>
2018-03-30 17:58:25 -0700

    added

---
+++ angular-src/src/app/view-deal-ads/view-deal-ads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-deal-ads',
+  templateUrl: './view-deal-ads.component.html',
+  styleUrls: ['./view-deal-ads.component.css']
+})
+export class ViewDealAdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
