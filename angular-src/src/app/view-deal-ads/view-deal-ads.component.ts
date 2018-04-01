d7803362eedd4934d8e8dd9e47a3a0c5691f6e83
Spencer <spencer0cornelia@gmail.com>
2018-04-01 10:02:10 -0700

    added working function of getting ads for user

--- angular-src/src/app/view-deal-ads/view-deal-ads.component.ts
+++ angular-src/src/app/view-deal-ads/view-deal-ads.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../services/auth.service';
+import { DealAdService } from '../services/dealAd.service';
+
+import { User } from '../models/User';
+
 @Component({
   selector: 'app-view-deal-ads',
   templateUrl: './view-deal-ads.component.html',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewDealAdsComponent implements OnInit {
 
-  constructor() { }
+  private currentUser: string;
+  private userType: string;
+  private investorUserType: boolean = false;
+  private wholesalerUserType: boolean = false;
+  private lenderUserType: boolean = false;
+
+  constructor(private authService: AuthService,
+              private dealAdService: DealAdService) { }
 
   ngOnInit() {
+    this.currentUser = this.authService.loggedInUser();
+    this.userType = this.authService.loggedInUserType();
+    if (this.userType == 'Investor') {
+      this.investorUserType = true;
+    } else if (this.userType == 'Wholesaler') {
+      this.wholesalerUserType = true;
+    } else {
+      this.lenderUserType = true;
+    }
+
+    this.getAds();
+  }
+
+  getAds() {
+    if (this.investorUserType) {
+      this.dealAdService.getDealAdsForInvestor(this.currentUser)
+        .subscribe((response) => {
+          console.log('response:', response);
+        }, (error) => {
+
+        })
+    } else {
+      this.dealAdService.getAllDeals()
+    }
   }
 
 }
