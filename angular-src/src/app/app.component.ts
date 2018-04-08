fe053d3867af7d4984e93398a31fba73352eb0fe
Spencer <spencer0cornelia@gmail.com>
2018-04-08 12:48:47 -0700

    added logic for determining navigation for loading

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, Event as RouterEvent, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,35 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // set the initial value to true to show loading spinner on first load
+  private loading = true;
+
   public constructor(private titleService: Title, private router: Router) {
     this.titleService.setTitle('Veeya');
-    router.events.subscribe((val) => {
-      if (val instanceof NavigationEnd) {
-        window.scrollTo(0,0);
-      }
-    })
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event);
+    });
   }
 
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+
+    }
+
+    if (event instanceof NavigationEnd) {
+      window.scrollTo(0,0);
+      this.loading = false;
+    }
+
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 
 }
