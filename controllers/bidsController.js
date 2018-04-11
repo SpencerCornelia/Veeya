64595fe2ad7418d38d4348596fb8a6cb174e26a5
Spencer <spencer0cornelia@gmail.com>
2018-04-11 16:35:14 -0700

    added endpoint for ending auction

--- controllers/bidsController.js
+++ controllers/bidsController.js
@@ -19,11 +19,6 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// :id == propertyId
-router.post('/:id', (req, res) => {
-
-});
-
 router.put('/openauction', (req, res) => {
   let propertyId = req.body.propertyId;
   let deadline = req.body.deadline;
@@ -40,6 +35,18 @@ router.put('/openauction', (req, res) => {
       } else {
         res.status(500).json(response);
       }
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    })
+});
+
+router.post('/endauction', (req, res) => {
+  let propertyId = req.body.propertyId;
+  let deadline = req.body.deadline;
+  Bid.endAuction(propertyId, deadline)
+    .then((response) => {
+      res.status(201).json(response);
     })
     .catch((error) => {
       res.status(500).json(error);
