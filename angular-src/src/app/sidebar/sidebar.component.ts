50b61ea096c9b6dfd2a7dcd820421c669db33b18
Spencer <spencer0cornelia@gmail.com>
2018-04-15 08:40:58 -0700

    added .gitignore to root

--- angular-src/src/app/sidebar/sidebar.component.ts
+++ angular-src/src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import fontawesome from '@fortawesome/fontawesome'
 
 import { User } from '../models/User';
 
