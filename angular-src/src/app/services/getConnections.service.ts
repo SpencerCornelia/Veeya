9b067b278eee8353af9bf9a10c9a97d45161675c
Spencer <spencer0cornelia@gmail.com>
2018-04-15 20:01:41 -0700

    used similar functionality for getting pending connections as I do getCurrentUser: checks if it exists. if it does not makes api call

--- angular-src/src/app/services/getConnections.service.ts
+++ angular-src/src/app/services/getConnections.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
+import { AuthService } from '../services/auth.service';
 import { User } from '../models/User';
 
 import 'rxjs/add/operator/map';
@@ -9,22 +10,42 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GetConnectionsService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private authService: AuthService) { }
+
+  private pendingConnections: any;
+  private observable: Observable<any>;
 
   private serverApi = 'http://localhost:3000';
 
-  public getConnectionsForUser(userId: String):Observable<User[]> {
+  public getConnectionsForUser(userId: string):Observable<User[]> {
     let URI = this.serverApi + `/user/connections/${userId}`;
     return this.http.get(URI)
       .map(res => res.json())
       .map(res => <User[]>res.data);
   }
 
-  public getPendingConnections(userId: String):Observable<User[]> {
-    let URI = this.serverApi + `/user/pendingconnections/${userId}`;
-    return this.http.get(URI)
-      .map(res => res.json())
-      .map(res => <User[]>res.data)
+  public getPendingConnections() {
+    if (this.pendingConnections) {
+      return Observable.of(this.pendingConnections);
+    } else if (this.observable) {
+      return this.observable;
+    } else {
+      let userId = this.authService.loggedInUser();
+      let URI = this.serverApi + `/user/pendingconnections/${userId}`;
+
+      this.observable = this.http.get(URI)
+        .map((response) => {
+          this.observable = null;
+          this.pendingConnections = response.json();
+          this.pendingConnections = this.pendingConnections.data;
+          return this.pendingConnections;
+        })
+        .catch((error) => {
+          return Observable.throw(error.json());
+        })
+        .share();
+        return this.observable;
+    }
   }
 
 }
