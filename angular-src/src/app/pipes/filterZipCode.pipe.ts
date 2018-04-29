9313db2cd4f1bebef63197dcf0dddccd768a3b00
Spencer <spencer0cornelia@gmail.com>
2018-04-28 18:32:21 -0700

    added for handling piping data from view deal ads component

---
+++ angular-src/src/app/pipes/filterZipCode.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filterZipCode'
+})
+
+export class FilterZipCodePipe implements PipeTransform {
+  transform(items: any[], searchText: string): any[] {
+    if(!items) return [];
+
+    if(!searchText) return items;
+
+    searchText = searchText.toLowerCase();
+
+    return items.filter((item) => {
+      if (item.zipCode) {
+        return item.zipCode.toLowerCase().includes(searchText);
+      }
+    });
+  }
+}
