d39933624674be5fff14d6ffa3e39fd4ad3a5c39
Spencer <spencer0cornelia@gmail.com>
2018-04-30 19:31:27 -0700

    added route for retrieving starred properties && replaced propertyId with entire property object in post route to star property

--- controllers/propertiesController.js
+++ controllers/propertiesController.js
@@ -218,10 +218,21 @@ router.post('/denysoldproperty', (req, res) => {
     })
 });
 
+router.get('/starproperties/:id', (req, res) => {
+  let investorId = req.params.id;
+  user.getStarredProperties(investorId)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
 router.post('/starproperty', (req, res) => {
   let investorId = req.body.investorId;
-  let propertyId = req.body.propertyId;
-  user.starProperty(investorId, propertyId)
+  let property = req.body.property;
+  user.starProperty(investorId, property)
     .then((response) => {
       res.status(201).json(response);
     })
