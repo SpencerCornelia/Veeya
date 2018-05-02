b5975b912398bb635d99dfd106f3ffe7c7220539
Spencer <spencer0cornelia@gmail.com>
2018-05-01 22:47:10 -0700

    removed endpoint for inviting lender

--- controllers/lenderController.js
+++ controllers/lenderController.js
@@ -15,30 +15,5 @@ router.get('/all', (req, res) => {
     });
 });
 
-router.post('/invitelender', (req,res) => {
-  let userId = req.body.invitee_id;
-  let lenderId = '';
-
-  user.registerInvitedUser(req.body)
-    .then((lender) => {
-      lenderId = String(lender.data._id);
-      return user.addLenderConnection(lenderId, userId);
-    })
-    .then((updatedUser) => {
-      return user.addUserConnectionForLender(userId, lenderId);
-    })
-    .then((response) => {
-      if (response.success) {
-        delete response.data.password;
-        res.status(201).json(response);
-      } else {
-        res.status(500).json(response);
-      }
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    })
-});
-
 
 module.exports = router;
